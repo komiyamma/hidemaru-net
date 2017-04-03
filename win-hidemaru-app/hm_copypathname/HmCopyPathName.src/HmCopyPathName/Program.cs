1c8a7f99d3ff417d5906174e9cbb4b749d5e1054
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-04-04 05:32:18 +0900

    vscode.life へとお引越し

---
+++ win-hidemaru-app/hm_copypathname/HmCopyPathName.src/HmCopyPathName/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HmCopyPathName
+{
+    class Program
+    {
+        [DllImport(@"C:\usr\hidemaru\HmSharedOutputPane.dll")]
+        private extern static void SetSharedMessage(String msg);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            var path = args[0];
+            Clipboard.SetText(path);
+
+            try
+            {
+                SetSharedMessage(path);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
