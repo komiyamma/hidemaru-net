55a4e4f8ce3ccce30749f6dd69e1c49baa3bc110
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-04-13 03:13:18 +0900

    HmWordListRegexCompressの高速版

---
+++ win-hidemaru-app/hm_wordlistregexcompress/HmWordListRegexCompressHighSpeed/HmWordListRegexCompress.src/HmWordListRegexCompress/HmWordListRegexCompress.cpp
@@ -0,0 +1,17 @@
+#include <EXTERN.h>               /* from the Perl distribution     */
+#include <perl.h>                 /* from the Perl distribution     */
+
+static PerlInterpreter *my_perl;  /***    The Perl interpreter    ***/
+
+int main(int argc, char **argv, char **env)
+{
+	PERL_SYS_INIT3(&argc, &argv, &env);
+	my_perl = perl_alloc();
+	perl_construct(my_perl);
+	PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+	perl_parse(my_perl, NULL, argc, argv, (char **)NULL);
+	perl_run(my_perl);
+	perl_destruct(my_perl);
+	perl_free(my_perl);
+	PERL_SYS_TERM();
+}
