0884abb304d430dda4de81f4880b5165c35d28c3
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-04-14 19:18:28 +0900

    日本語ルビ分析。Win8.1以上

---
+++ win-hidemaru-app/hm_japanesephonetic_analyzer/HmJapanesePhoneticAnalyzer.src/HmJapanesePhoneticAnalyzer/HmJapanesePhoneticAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class HmJapanesePhoneticAnalyzer
+{
+    string m_text = "";
+    int m_char_ix = 0;
+
+    public HmJapanesePhoneticAnalyzer() { }
+
+    public List<JapanesePhoneticAnalyzeData> GetJapanesePhoneticAnalyzeDataList(String text)
+    {
+        this.m_char_ix = 0;
+        this.m_text = text;
+        japanesePhoneticAnalyzeDataList.Clear();
+
+        var list = Windows.Globalization.JapanesePhoneticAnalyzer.GetWords(text, true);
+        foreach (var data in list)
+        {
+            var japanesePhoneticAnalyzeData = new JapanesePhoneticAnalyzeData();
+
+            // 読みはそのまま入れる
+            japanesePhoneticAnalyzeData.YomiText = data.YomiText;
+
+            // 元の文字のどこからどこまでをコピーすればいいのかを勘定
+            // 元の文字列を使う必要性がある理由は、Windows.Globalization.JapanesePhoneticAnalyzerが自動で文字列を全角にしてしまうための対処
+            var len = data.DisplayText.Length;
+            var start = m_char_ix;
+            m_char_ix = m_char_ix + len;
+            japanesePhoneticAnalyzeData.DisplayText = m_text.Substring(start, len);
+
+            // 
+            japanesePhoneticAnalyzeData.IsPhraseStart = IsContainsKanji(japanesePhoneticAnalyzeData.DisplayText);
+            japanesePhoneticAnalyzeDataList.Add(japanesePhoneticAnalyzeData);
+        }
+
+        // ExecuteAnalyze(m_text);
+        return japanesePhoneticAnalyzeDataList;
+    }
+
+    public class JapanesePhoneticAnalyzeData
+    {
+        public string DisplayText;
+        public bool IsPhraseStart;
+        public string YomiText;
+    }
+
+    List<JapanesePhoneticAnalyzeData> japanesePhoneticAnalyzeDataList = new List<JapanesePhoneticAnalyzeData>();
+
+    bool IsContainsKanji(string text)
+    {
+        //漢字が含まれているか調べる
+        if (Regex.IsMatch(text,
+            @"[\p{IsCJKUnifiedIdeographs}" +
+            @"\p{IsCJKCompatibilityIdeographs}" +
+            @"\p{IsCJKUnifiedIdeographsExtensionA}]|" +
+            @"[\uD840-\uD869][\uDC00-\uDFFF]|\uD869[\uDC00-\uDEDF]"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
