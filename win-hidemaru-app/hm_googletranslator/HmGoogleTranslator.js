2b0093c96bd7e8f3709c53c29defa99194e1bb98
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-05-21 22:30:21 +0900

    汎用性をもった翻訳アルゴリズム

--- win-hidemaru-app/hm_googletranslator/HmGoogleTranslator.js
+++ win-hidemaru-app/hm_googletranslator/HmGoogleTranslator.js
@@ -1,48 +1,5 @@
-///<reference path="hmv8.d.ts"/>
-/**
- * ストラテジーっぽい翻訳アルゴリズムの抽象クラス。
- */
-class ITranslatorAlgorithm {
-    get QueryParams() {
-        return this.queryParams;
-    }
-    constructor(targetLanguages) {
-        let webclr = host.lib("System.Web");
-        this.HttpUtility = webclr.System.Web.HttpUtility;
-        this.queryParams = new clr.System.Collections.Specialized.NameValueCollection();
-        this.targetLanguages = targetLanguages;
-        this.InitializeQueryParams();
-    }
-    get Url() {
-        throw "NotImplementsException";
-    }
-    /**
-     * デフォルトは通常選択や行選択に対するテキスト。
-     * 矩形選択も対象にしたければ、少し記述を足す必要があるが、あまり機会は無いだろう。
-     */
-    get SrcText() {
-        return hm.Edit.SelectedText;
-    }
-    /**
-     * 結果を反映する時のフォーマット。
-     * どのような文字列フォーマットで結果を足しこんでいくか。
-     */
-    set DstText(value) {
-        hm.Edit.SelectedText = `${this.SrcText}\n${value}\n`;
-    }
-    get Method() {
-        throw "NotImplementsException";
-    }
-    /**
-     * そのサイトへと要求するクエリーパラメータ
-     */
-    InitializeQueryParams() {
-        throw "NotImplementsException";
-    }
-    FilterResultText(html) {
-        throw "NotImplementsException";
-    }
-}
+///<reference path="HmV8.d.ts"/>
+///<reference path="HmAbstractTranslator.ts"/>
 /**
  * Google用に導出されたクラス
  */
@@ -67,17 +24,18 @@ class GoogleTranslatorAlgorithmStrategy extends ITranslatorAlgorithm {
         return "http://translate.google.com/translate_t";
     }
     /**
-     * 結果のHTMLページから対象の部分を抽出するどろ臭い部分
-     * @param html
+     * 結果のページから対象の部分を抽出するどろ臭い部分
+     * Googleの場合は、HTML。
+     * @param result_page
      */
-    FilterResultText(html) {
+    FilterResultText(result_page) {
         // id=result_boxあたりのタグ内のInnerTextが翻訳語の文字列。
         // 本格的にやるなら「HtmlAgilityPack」で、対象のidタグのInnerTextを求めるのが良いが大仰である。
         // 正規表現で対処してしまって良いだろう。
         // 複数行考慮
         let resultAroundTextRegexp = /<span id=result_box [\s\S]+?<\/span><\/span>/;
         // 結果のHTMLページ全体から、翻訳結果周辺をまずは抽出。
-        let resultAroundResultArray = resultAroundTextRegexp.exec(html);
+        let resultAroundResultArray = resultAroundTextRegexp.exec(result_page);
         // 存在する
         if (resultAroundResultArray) {
             // hm.Edit.TotalText = resultAroundResultArray[0];
@@ -87,7 +45,7 @@ class GoogleTranslatorAlgorithmStrategy extends ITranslatorAlgorithm {
             // さらにタグを全部除去。複数行考慮
             regexpResultText = regexpResultText.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
             // 結果文字列がHTMLエンコードされてるのでHTMLデコードする。
-            // (ただし元々がHTMLエンコードした状態の文字列と区別が付かないという不正確さがある)
+            // (ただし元々がHTMLエンコードした状態の文字列だと区別が付かないという不正確さがある)
             regexpResultText = this.HttpUtility.HtmlDecode(regexpResultText);
             return regexpResultText;
         }
@@ -96,85 +54,6 @@ class GoogleTranslatorAlgorithmStrategy extends ITranslatorAlgorithm {
         }
     }
 }
-/**
- * メイン処理的な翻訳全体の大流アルゴリズム
- * 「Web翻訳とは何か」といった共通観念的処理が記載されている。
- */
-class ContextTranslator {
-    /**
-     * @param strategy : 特定の翻訳戦略アルゴリズムを受け取る
-     */
-    constructor(strategy) {
-        this.strategy = strategy;
-        this.webclient = new clr.System.Net.WebClient();
-    }
-    /**
-     * 大流としての翻訳。
-     * WebClientを使って、strategyの各種パラメータを使ってクエリーをWebへと投げる。
-     * 返ってきた値を、stragetyのフィルター方法に従って最終系にする。
-     */
-    Translate() {
-        // 翻訳元のデータがないってのはさすがにダメすぎでしょう。
-        if (!this.strategy.SrcText) {
-            PrintOutputPane("翻訳対象が存在していません。\r\n(対象テキストを選択してない等)\r\n");
-            return;
-        }
-        // クエリを発行した結果の受信データ全体。型はcli::array<Byte>^ だが、面倒くさいのでanyで
-        let resData = this.GetRequestQueryData();
-        if (!resData) {
-            PrintOutputPane("翻訳結果をWebへとリクエストしましたが、失敗しました。\r\n");
-            return;
-        }
-        // 結果のデータの塊はUTF8のテキストとみなして変換
-        let resText = clr.System.Text.Encoding.UTF8.GetString(resData);
-        // hm.Edit.TotalText = resText;
-        // 結果のテキストを、戦略アルゴリズムが持つ「結果フィルターメソッド」にかける
-        let dstText = this.strategy.FilterResultText(resText);
-        // 翻訳後のテキストを該当の戦略アルゴリズムのDstTextとして設定。
-        this.strategy.DstText = dstText;
-    }
-    GetRequestQueryData() {
-        // 戦略アルゴリズムが持つクエリーパラメータ
-        let queryParams = this.strategy.QueryParams;
-        // 戦略のクエリー手段(GET/POST)
-        let method = this.strategy.Method;
-        // メソッドがPOSTとGETで少々要求メソッドや設定方法が違うので、ちょっとブサだがswitch
-        switch (method) {
-            case "POST": {
-                this.webclient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
-                return this.webclient.UploadValues(this.strategy.Url, queryParams);
-            }
-            case "GET": {
-                this.webclient.QueryString = queryParams;
-                return this.webclient.DownloadData(this.strategy.Url);
-            }
-        }
-    }
-}
-/**
- * 秀丸用のアウトプットパネル。簡易
- */
-function PrintOutputPane(message) {
-    // tslint:disable-next-line:no-string-literal
-    hm.Macro.Var["$OutputPaneMessageText"] = message;
-    hm.Macro.Eval(f => {
-        /*
-            #HMOP = loaddll(hidemarudir + @"\HmOutputPane.dll");
-            #r = dllfunc(#HMOP, "Output", hidemaruhandle(0), $OutputPaneMessageText + "\r\n");
-            freedll( #HMOP );
-        */
-    });
-}
-// 特定の翻訳戦略アルゴリズムや、言語指定パラメータを引数にする。
-function Main(ctor, langParams) {
-    // 特定の翻訳戦略アルゴリズムインスタンスを構築
-    // ちょっとファクトリっぽい
-    let strategy = new ctor(langParams);
-    // 個別戦略にとらわれない翻訳トランスレーター大流処理インスタンス
-    let translator = new ContextTranslator(strategy);
-    // 翻訳
-    translator.Translate();
-}
 // 何語から何語なのか
-let langParams = { src: "en", dst: "ja" };
-Main(GoogleTranslatorAlgorithmStrategy, langParams);
+// let langParams: ITranslatorLanguageParams = { src: "en", dst: "ja" };
+// let result_words: string = ContextTranslator.Translate(GoogleTranslatorAlgorithmStrategy, langParams); 
---
+++ win-hidemaru-app/hm_googletranslator/HmGoogleTranslator.js.src/HmAbstractTranslator.ts
@@ -0,0 +1,203 @@
+///<reference path="HmV8.d.ts"/>
+
+type TranslatorLanguageType = undefined | "en" | "ja";
+
+type QueryMethodType = "POST" | "GET";
+
+/**
+ * 翻訳対象の言語のSource(From)とDestination(To)
+ */
+interface ITranslatorLanguageParams {
+    src: TranslatorLanguageType;
+    dst: TranslatorLanguageType;
+}
+
+/**
+ * 翻訳戦略インターフェイスのコンストラクタ専用インターフェイス
+ * ファクトリ系を作る際にはTypeScriptでは分ける必要がある。
+ */
+interface ITranslatorAlgorithmStrategyConstructor {
+    new (targetLanguages: ITranslatorLanguageParams);
+}
+/**
+ * 翻訳戦略のインターフェイス
+ */
+interface ITranslatorAlgorithmStrategy {
+    FilterResultText: (html: string) => string;
+    QueryParams: INameValueCollection;
+    Url: string;
+    Method: QueryMethodType;
+    SrcText: string;
+    DstText: string;
+}
+
+/**
+ * ストラテジーっぽい翻訳アルゴリズムの抽象クラス。
+ */
+abstract class ITranslatorAlgorithm implements ITranslatorAlgorithmStrategy {
+
+    // クエリーする際のkeyとvalueの組み合わせ
+    protected queryParams: INameValueCollection;
+    get QueryParams(): INameValueCollection {
+        return this.queryParams;
+    }
+
+    // 翻訳対象の言語のsrcとdst
+    protected targetLanguages: ITranslatorLanguageParams;
+
+    // .NETのHttpUtilityは何かとつかうだろうからショートカット用意
+    protected HttpUtility: IHttpUtility;
+
+    constructor(targetLanguages: ITranslatorLanguageParams) {
+        let webclr: any = host.lib("System.Web");
+        this.HttpUtility = webclr.System.Web.HttpUtility;
+
+        this.queryParams = new clr.System.Collections.Specialized.NameValueCollection();
+        this.targetLanguages = targetLanguages;
+
+        this.InitializeQueryParams();
+    }
+
+    get Url(): string {
+        throw "NotImplementsException";
+    }
+
+    /**
+     * デフォルトは通常選択や行選択に対するテキスト。
+     * 矩形選択も対象にしたければ、少し記述を足す必要があるが、あまり機会は無いだろう。
+     */
+    get SrcText(): string {
+        return hm.Edit.SelectedText;
+    }
+
+    /**
+     * 結果を反映する時のフォーマット。
+     * どのような文字列フォーマットで結果を足しこんでいくか。
+     */
+    set DstText(value: string) {
+        hm.Edit.SelectedText = `${this.SrcText}\n${value}\n`;
+    }
+
+    /**
+     * メソッド。通常GETかPOSTを想定。将来的にAUTHを足してもいいかもしれない。
+     */
+    get Method(): QueryMethodType {
+        throw "NotImplementsException";
+    }
+
+    /**
+     * そのサイトへと要求するクエリーパラメータ
+     */
+    protected InitializeQueryParams(): void {
+        throw "NotImplementsException";
+    }
+
+    /**
+     * 結果のページから対象の部分を抽出するどろ臭い部分
+     * HTMLかもしれないし、XMLかもしれないし、JSONかもしれない。
+     * 検索エンジンによって異なるだろう。
+     */
+    FilterResultText(result_page: string): string {
+        throw "NotImplementsException";
+    }
+}
+
+/**
+ * メイン処理的な翻訳全体の大流アルゴリズム
+ * 「Web翻訳とは何か」といった共通観念的処理が記載されている。
+ */
+class ContextTranslator {
+    protected strategy: ITranslatorAlgorithmStrategy;
+    protected webclient: IWebClient;
+
+    /**
+     * @param strategy : 特定の翻訳戦略アルゴリズムを受け取る
+     */
+    protected constructor(strategy: ITranslatorAlgorithmStrategy) {
+        this.strategy = strategy;
+        this.webclient = new clr.System.Net.WebClient();
+    }
+
+    /**
+     * 大流としての翻訳。
+     * WebClientを使って、strategyの各種パラメータを使ってクエリーをWebへと投げる。
+     * 返ってきた値を、stragetyのフィルター方法に従って最終系にする。
+     */
+    protected GetTranslateResult(): string {
+        // 翻訳元のデータがないってのはさすがにダメすぎでしょう。
+        if (!this.strategy.SrcText) {
+            PrintOutputPane("翻訳対象が存在していません。\r\n(対象テキストを選択してない等)\r\n");
+            return;
+        }
+
+        // クエリを発行した結果の受信データ全体。型はcli::array<Byte>^ だが、面倒くさいのでanyで
+        let resData: any = this.GetRequestQueryData();
+        if (!resData) {
+            PrintOutputPane("翻訳結果をWebへとリクエストしましたが、失敗しました。\r\n");
+            return;
+        }
+
+        // 結果のデータの塊はUTF8のテキストとみなして変換
+        let resText: string = clr.System.Text.Encoding.UTF8.GetString(resData);
+        // hm.Edit.TotalText = resText;
+        // 結果のテキストを、戦略アルゴリズムが持つ「結果フィルターメソッド」にかける
+        let dstText: string = this.strategy.FilterResultText(resText);
+
+        // 翻訳後のテキストを該当の戦略アルゴリズムのDstTextとして設定。
+        return dstText;
+    }
+
+    protected GetRequestQueryData(): any {
+        // 戦略アルゴリズムが持つクエリーパラメータ
+        let queryParams =  this.strategy.QueryParams;
+
+        // 戦略のクエリー手段(GET/POST)
+        let method: QueryMethodType = this.strategy.Method;
+
+        // メソッドがPOSTとGETで少々要求メソッドや設定方法が違うので、ちょっとブサだがswitch
+        switch (method) {
+            case "POST": {
+                this.webclient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                return this.webclient.UploadValues(this.strategy.Url, queryParams);
+            }
+            case "GET": {
+                this.webclient.QueryString = queryParams;
+                return this.webclient.DownloadData(this.strategy.Url);
+            }
+        }
+    }
+
+    // 特定の翻訳戦略アルゴリズムや、言語指定パラメータを引数にする。
+    static Translate(ctor: ITranslatorAlgorithmStrategyConstructor, langParams: ITranslatorLanguageParams): string {
+
+        // 特定の翻訳戦略アルゴリズムインスタンスを構築
+        // ちょっとファクトリっぽい
+        let strategy: ITranslatorAlgorithmStrategy = new ctor(langParams);
+
+        // 個別戦略にとらわれない翻訳トランスレーター大流処理インスタンス
+        let translator: ContextTranslator = new ContextTranslator(strategy);
+
+        // 翻訳結果を取得し、反映
+        let result: string = translator.GetTranslateResult();
+
+        strategy.DstText = result;
+
+        return result; 
+    }
+    
+}
+
+/**
+ * 秀丸用のアウトプットパネル。簡易
+ */
+function PrintOutputPane(message: string): void {
+    // tslint:disable-next-line:no-string-literal
+    hm.Macro.Var["$OutputPaneMessageText"] = message;
+    hm.Macro.Eval(f => {
+        /*
+            #HMOP = loaddll(hidemarudir + @"\HmOutputPane.dll");
+            #r = dllfunc(#HMOP, "Output", hidemaruhandle(0), $OutputPaneMessageText + "\r\n");
+            freedll( #HMOP );
+        */
+    });
+}
