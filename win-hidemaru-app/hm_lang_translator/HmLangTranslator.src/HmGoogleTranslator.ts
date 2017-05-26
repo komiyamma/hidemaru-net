bbbe4806d8493b85574a9c9cc51b575beee1cb7b
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-05-26 13:57:05 +0900

    元へと戻す。新規のJSON.parseを使えるURLはIPによっては弾かれることが判明した。

--- win-hidemaru-app/hm_lang_translator/HmLangTranslator.src/HmGoogleTranslator.ts
+++ win-hidemaru-app/hm_lang_translator/HmLangTranslator.src/HmGoogleTranslator.ts
@@ -2,76 +2,61 @@
 ///<reference path="HmAbstractTranslator.ts"/>
 /**
  * HmGoogleTranslator v1.02
- * Copyright (C) 2017 VSCode.life
+ * Copyright (C) @ 2017 VSCode.life
  */
-
 /**
  * Google用に導出されたクラス
  */
 class GoogleTranslatorQueryStrategy extends AbstractTranslatorQueryStrategy {
-
-    protected InitializeQueryParams(): void {
+    InitializeQueryParams() {
         // 翻訳対象の元テキスト。
         this.QueryParams.Add("text", this.SrcText);
-
-        // これが必要。これがあれば、答えのテキストだけで返ってくる。
-        this.QueryParams.Add("client", "j");
-
-        // 何語から(source language)
-        this.QueryParams.Add("sl", <string>this.TargetLanguages.src);
-
-        // 何語へ(target language)
-        this.QueryParams.Add("tl", <string>this.TargetLanguages.dst);
-
+        // 何語から何語へ翻訳するのか
+        this.QueryParams.Add("langpair", `${this.TargetLanguages.src}|${this.TargetLanguages.dst}`);
         // 結果ページで使用される言語
         // (翻訳内容とは無関係で、GUIインターフェイスの言語。日本語で良い)
         this.QueryParams.Add("hl", "ja");
-
         // 入力文字列の文字コード
         this.QueryParams.Add("ie", "UTF8");
-
         // 結果文字列の文字コード
         this.QueryParams.Add("oe", "UTF8");
     }
-
-    get Method(): QueryMethodType {
+    get Method() {
         return "GET"; // GETは動作するがPOSTはGoogle側が受け付けなくなった？
     }
-
-    get Url(): string {
-        return "http://translate.google.com/translate_a/t";
+    get Url() {
+        return "http://translate.google.com/translate_t";
     }
-
     /**
-     *  @param result_page: 結果のページはJavaScriptの文字列フォーマット、
-     *  もしくはJavaScriptの文字列の配列によって格納されてる。
-     *  "りんご"
-     *    という形かもしくは
-     *  ["りんご", "en"]
-     *    という形。
-     *  ２番めの要素には、翻訳元の言語をautoで自動判断させた際に、何語と自動判断されたのか、が入る。
+     * 結果のページから対象の部分を抽出するどろ臭い部分
+     * Googleの場合は、HTML。
+     * @param result_page
      */
-    FilterResultText(result_page: string): string {
-
-        let page_text: string = result_page;
-
-        // 具体的に示されている時には、jsonの体をしていないので、[ ] でjson的な形にしてしまう。
-        if (!result_page.startsWith("[")) {
-            page_text = "[" + page_text + "]";
+    FilterResultText(result_page) {
+        // id=result_boxあたりのタグ内のInnerTextが翻訳語の文字列。
+        // 本格的にやるなら「HtmlAgilityPack」で、対象のidタグのInnerTextを求めるのが良いが大仰である。
+        // 正規表現で対処してしまって良いだろう。
+        // 複数行考慮
+        let resultAroundTextRegexp = /<span id=result_box [\s\S]+?<\/span><\/(span|div)>/;
+        // 結果のHTMLページ全体から、翻訳結果周辺をまずは抽出。
+        let resultAroundResultArray = resultAroundTextRegexp.exec(result_page);
+        // 存在する
+        if (resultAroundResultArray) {
+            // 得た「周辺テキスト」にたいして…
+            let regexpResultText = resultAroundResultArray[0];
+            // さらにタグを全部除去。複数行考慮
+            regexpResultText = regexpResultText.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
+            // 結果文字列がHTMLエンコードされてるのでHTMLデコードする。
+            // (ただし元々がHTMLエンコードした状態の文字列だと区別が付かないという不正確さがある)
+            regexpResultText = this.HttpUtility.HtmlDecode(regexpResultText);
+            return regexpResultText;
         }
-
-        // フォーマットが本当に合っているのか裏付けが薄いのでtry...catchしておく。
-        try {
-            let res: any = JSON.parse(page_text);
-            return res[0];
-        } catch(err) {
-            PrintOutputPane(err);
+        else {
+            return "";
         }
-        return "";
     }
 }
-
-
 // 何語から何語なのか
 // let langParams: ITranslatorLanguageParams = { src: "en", dst: "ja" };
-// let result_words: string = ContextTranslator.Translate(GoogleTranslatorQueryStrategy, langParams);
+// let result_words: string = ContextTranslator.Translate(GoogleTranslatorQueryStrategy, langParams); 
+
