ea00a95ad36122f5cd43bbd92499dcf6322a6716
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-06-25 23:39:54 +0900

    関数等に対応

--- win-hidemaru-app/hm_excel_file_loader/HmExcelFileLoader.src/HmExcelFileLoader/Program.cs
+++ win-hidemaru-app/hm_excel_file_loader/HmExcelFileLoader.src/HmExcelFileLoader/Program.cs
@@ -66,9 +66,97 @@ public class HmExcelFileLoader
                 if (cell == null)
                 {
                     loaded_row.Add("");
-                } else
+                }
+                else
                 {
-                    loaded_row.Add(cell.ToString());
+                    string cellStr = "";
+                    switch (cell.CellType)
+                    {
+                        // 文字列型
+                        case CellType.String:
+                            cellStr = cell.StringCellValue;
+                            break;
+                        // 数値型（日付の場合もここに入る）
+                        case CellType.Numeric:
+                            // セルが日付情報が単なる数値かを判定
+                            if (DateUtil.IsCellDateFormatted(cell))
+                            {
+                                // 日付型
+                                // 本来はスタイルに合わせてフォーマットすべきだが、
+                                // うまく表示できないケースが若干見られたので固定のフォーマットとして取得
+                                cellStr = cell.DateCellValue.ToString("yyyy/MM/dd HH:mm:ss");
+                            }
+                            else
+                            {
+                                // 数値型
+                                cellStr = cell.NumericCellValue.ToString();
+                            }
+                            break;
+
+                        // bool型(文字列でTrueとか入れておけばbool型として扱われた)
+                        case CellType.Boolean:
+                            cellStr = cell.BooleanCellValue.ToString();
+                            break;
+
+                        // 入力なし
+                        case CellType.Blank:
+                            cellStr = cell.ToString();
+                            break;
+
+                        // 数式
+                        case CellType.Formula:
+                            // 下記で数式の文字列が取得される
+                            //cellStr = cell.CellFormula.ToString();
+
+                            // 数式の元となったセルの型を取得して同様の処理を行う
+                            // コメントは省略
+                            switch (cell.CachedFormulaResultType)
+                            {
+                                case CellType.String:
+                                    cellStr = cell.StringCellValue;
+                                    break;
+                                case CellType.Numeric:
+
+                                    if (DateUtil.IsCellDateFormatted(cell))
+                                    {
+                                        cellStr = cell.DateCellValue.ToString("yyyy/MM/dd HH:mm:ss");
+                                    }
+                                    else
+                                    {
+                                        cellStr = cell.NumericCellValue.ToString();
+                                    }
+                                    break;
+                                case CellType.Boolean:
+                                    cellStr = cell.BooleanCellValue.ToString();
+                                    break;
+                                case CellType.Blank:
+                                    break;
+                                case CellType.Error:
+                                    cellStr = cell.ErrorCellValue.ToString();
+                                    break;
+                                case CellType.Unknown:
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+
+                        // エラー
+                        case CellType.Error:
+                            cellStr = cell.ErrorCellValue.ToString();
+                            break;
+
+                        // 型不明なセル
+                        case CellType.Unknown:
+                            cellStr = cell.ToString();
+                            break;
+                        // もっと不明なセル（あぶない刑事をなぜか思い出しました）
+                        default:
+                            cellStr = cell.ToString();
+                            break;
+
+                    }
+                    loaded_row.Add(cellStr);
                 }
             }
 
@@ -129,7 +217,8 @@ public class HmExcelFileLoader
                 ReadXLSX(strExcelFileFullPath);
             }
 
-        } catch(Exception e)
+        }
+        catch (Exception e)
         {
             System.Windows.Forms.MessageBox.Show(e.Message);
         }
