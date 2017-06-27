2285fa5c37635b51bcdcf3df867cb3d6fca99b4d
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-06-27 23:18:27 +0900

    このファイル要らなそう。キモイがJNIが直接dll内のextern名決め打ちで探しに来るので…

--- win-hidemaru-app/hm_javavm/hmJavaVM/java_native_methods.h
+++ win-hidemaru-app/hm_javavm/hmJavaVM/java_native_methods.h
@@ -2,5 +2,3 @@
 
 #include <jni.h>
 
-extern "C" JNIEXPORT jstring JNICALL Java_Hm_concat(JNIEnv *, jobject, jstring, jstring);
-extern "C" JNIEXPORT jstring JNICALL Java_Hm_MacroVarGetObj(JNIEnv *, jobject, jstring);
