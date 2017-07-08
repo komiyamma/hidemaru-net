44c8f89d80193fbbeda35321814c17cc2037fd31
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-07-08 13:24:49 +0900

    GetCursorPosFromMousePosの追加

--- win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmHidemaruEdit.cs
+++ win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmHidemaruEdit.cs
@@ -53,16 +53,17 @@ public partial class hmLmDynamicLib
                 int success = pGetCursorPosUnicode(ref lineno, ref column);
                 if (success > 0)
                 {
-                    LuaTable p = new LuaTable();
+                    var p = new LuaTable();
                     p["lineno"] = lineno;
                     p["column"] = column;
                     return p;
                 }
-
-                var t = new LuaTable();
-                t["column"] = -1;
-                t["lineno"] = -1;
-                return t;
+                else {
+                    var p = new LuaTable();
+                    p["column"] = -1;
+                    p["lineno"] = -1;
+                    return p;
+                }
             }
 
             // columnやlinenoはエディタ的な座標である。
