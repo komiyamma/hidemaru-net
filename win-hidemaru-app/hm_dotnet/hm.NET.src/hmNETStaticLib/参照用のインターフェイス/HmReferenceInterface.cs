05a7c0ed9e5cf97d1f0afa6200c251d5e29d3944
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-07-12 15:49:21 +0900

    独自クラスを消去して依存性を低くした

--- win-hidemaru-app/hm_dotnet/hm.NET.src/hmNETStaticLib/参照用のインターフェイス/HmReferenceInterface.cs
+++ win-hidemaru-app/hm_dotnet/hm.NET.src/hmNETStaticLib/参照用のインターフェイス/HmReferenceInterface.cs
@@ -4,84 +4,112 @@ namespace Hidemaru
 {
     public static class Hm
     {
-        public static IntPtr GetWindowHandle()
+        public static IntPtr WindowHandle
         {
-            return hmNETDynamicLib.Hidemaru.WindowHandle;
+            get
+            {
+                return hmNETDynamicLib.Hidemaru.WindowHandle;
+            }
         }
 
-        public static Double GetVersion()
+        public static Double Version
         {
-            return hmNETDynamicLib.Hidemaru.version;
+            get {
+                return hmNETDynamicLib.Hidemaru.version;
+            }
         }
 
-        public class Macro
+        public static class Macro
         {
-            public static int DoEval(String expression)
+            public static int Eval(String expression)
             {
                 return hmNETDynamicLib.Hidemaru.Macro.Eval(expression);
             }
-            public static Object GetVar(String var_name)
-            {
-                return hmNETDynamicLib.Hidemaru.Macro.Var[var_name];
-            }
-            public static Boolean SetVar(String var_name, Object value)
-            {
-                hmNETDynamicLib.Hidemaru.Macro.Var[var_name] = value;
-                return true;
+
+            public static TVar Var = new TVar();
+            public class TVar {
+                public Object this[String var_name] {
+                    get
+                    {
+                        return hmNETDynamicLib.Hidemaru.Macro.Var[var_name];
+                    }
+                    set
+                    {
+                        hmNETDynamicLib.Hidemaru.Macro.Var[var_name] = value;
+                    }
+                }
             }
         }
 
         public static class Edit
         {
-            public static String GetFileFullPath()
+            public static String FilePath
             {
-                String file_name = hmNETDynamicLib.Hidemaru.Edit.FileName;
-                if (String.IsNullOrEmpty(file_name))
+                get
                 {
-                    return null;
-                } else {
-                    return file_name;
+                    String file_name = hmNETDynamicLib.Hidemaru.Edit.FileName;
+                    if (String.IsNullOrEmpty(file_name))
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return file_name;
+                    }
                 }
             }
 
-            public static String GetTotalText()
+            public static String TotalText
             {
-                return hmNETDynamicLib.Hidemaru.Edit.TotalText;
-            }
-            public static Boolean SetTotalText(String text)
-            {
-                hmNETDynamicLib.Hidemaru.Edit.TotalText = text;
-                return true;
-            }
-            public static String GetSelectedText()
-            {
-                return hmNETDynamicLib.Hidemaru.Edit.SelectedText;
-            }
-            public static Boolean SetSelectedText(String text)
-            {
-                hmNETDynamicLib.Hidemaru.Edit.SelectedText = text;
-                return true;
-            }
-            public static String GetLineText()
-            {
-                return hmNETDynamicLib.Hidemaru.Edit.LineText;
-            }
-            public static Boolean SetLineText(String text)
-            {
-                hmNETDynamicLib.Hidemaru.Edit.LineText = text;
-                return true;
+                get
+                {
+                    return hmNETDynamicLib.Hidemaru.Edit.TotalText;
+                }
+                set
+                {
+                    hmNETDynamicLib.Hidemaru.Edit.TotalText = value;
+                }
             }
 
-            public static (int LineNo, int Column) GetCursorPos()
+            public static String SelectedText
             {
-                var pos = hmNETDynamicLib.Hidemaru.Edit.CursorPos;
-                return (pos.lineno, pos.column);
+                get
+                {
+                    return hmNETDynamicLib.Hidemaru.Edit.SelectedText;
+                }
+                set
+                {
+                    hmNETDynamicLib.Hidemaru.Edit.SelectedText = value;
+                }
             }
 
-            public static (int LineNo, int Column) GetCursorPosFromMousePos()
+            public static String LineText
             {
-                var pos = hmNETDynamicLib.Hidemaru.Edit.CursorPosFromMousePos;
-                return (pos.lineno, pos.column);
+                get {
+                    return hmNETDynamicLib.Hidemaru.Edit.LineText;
+                }
+
+                set
+                {
+                    hmNETDynamicLib.Hidemaru.Edit.LineText = value;
+                }
+
+            }
+
+            public static (int LineNo, int Column) CursorPos
+            {
+                get {
+                    var pos = hmNETDynamicLib.Hidemaru.Edit.CursorPos;
+                    return (pos.lineno, pos.column);
+                }
+            }
+
+            public static (int LineNo, int Column, int X, int Y) MousePos
+            {
+                get {
+                    var pos = hmNETDynamicLib.Hidemaru.Edit.MousePos;
+                    return (pos.lineno, pos.column, pos.x, pos.y);
+                }
             }
 
         }
