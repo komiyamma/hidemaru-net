784e9960dfc4e53834816337d659cb0ae355797f
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-07-19 21:50:35 +0900

    hm.NET上で動作するように変更。

---
+++ win-hidemaru-app/hm_textsynchrome/HmTextSynChrome/HmTextSynChrome/HmTextSynChromeForm.cs
@@ -0,0 +1,156 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Hidemaru;
+
+internal sealed class HmTextSynChromeForm : System.Windows.Forms.Form
+{
+    public static HmTextSynChromeForm form;
+
+    private IntPtr hWndHidemaru = IntPtr.Zero;
+    private string strPrvTotalText = "";
+    private string strOption1 = "--lang=ja";
+    private Timer update;
+
+    private IWebDriver driver;
+    private ChromeOptions chromeOptions;
+    private ChromeDriverService chromeService;
+    private bool is_driver_ready = false;
+
+
+    public HmTextSynChromeForm(System.IntPtr hWndHidemaru, string strOption1)
+    {
+        this.hWndHidemaru = hWndHidemaru;
+        this.strOption1 = strOption1;
+
+        SetFormAttribute();
+        SetWebBrowserAttribulte();
+        SetTimerAttribute();
+    }
+
+
+    /// <summary>フォームの属性設定</summary>
+    private void SetFormAttribute()
+    {
+
+        // 効果なさそうだが、一応Waitカーソルは使わない
+        Application.UseWaitCursor = false;
+    }
+
+    /// <summary>Webブラウザ属性設定</summary>
+    private void SetWebBrowserAttribulte()
+    {
+
+        try
+        {
+            chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument(strOption1);
+            chromeService = ChromeDriverService.CreateDefaultService();
+            chromeService.HideCommandPromptWindow = true;
+
+            driver = new ChromeDriver(chromeService, chromeOptions);
+            driver.Url = "about:blank";
+            is_driver_ready = true;
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.Message);
+        }
+    }
+
+    private void SetTimerAttribute()
+    {
+        // １秒に１回の更新で十分だろう。
+        update = new System.Windows.Forms.Timer();
+        update.Interval = 1000 * 1;
+        update.Tick += update_Tick;
+        update.Start();
+
+        update_Tick(null, null);
+    }
+
+
+    private void TaskMethod()
+    {
+        string curTotalText = Hm.Edit.TotalText;
+        if (curTotalText != strPrvTotalText)
+        {
+            strPrvTotalText = curTotalText;
+
+            var body = driver.FindElement(By.TagName("body"));
+
+            // var executor = driver as IJavaScriptExecutor;
+            IJavaScriptExecutor executor = driver as IJavaScriptExecutor;
+
+
+            // まず、JavaScriptによるEvaluateの予定なので、それを邪魔する要素は置き換え
+            string encodeText = curTotalText.Replace(@"\", @"\\");
+            encodeText = encodeText.Replace("'", @"\'").Replace(@"""", @"\""");
+
+            // 改行を糾合
+            encodeText = encodeText.Replace("\r\n", "\r");
+            encodeText = encodeText.Replace("\n", "\r");
+            encodeText = encodeText.Replace("\r", @"\n");
+
+            // JavaScriptとして評価
+            executor.ExecuteScript("arguments[0].innerText = '" + encodeText + "'", body);
+        }
+    }
+
+    private List<Task> TaskList = new List<Task>();
+    private void update_Tick(object sender, EventArgs e)
+    {
+        if (!is_driver_ready)
+        {
+            return;
+        }
+        try
+        {
+            // 非同期で結果を秀丸へと反映するためのタスクを生成
+            Task task = Task.Factory.StartNew(new Action(TaskMethod));
+
+            // リストの中で終わってるタスクはクリア
+            for (int ix = TaskList.Count - 1; ix >= 0; ix--)
+            {
+                if (TaskList[ix].IsCompleted)
+                {
+                    TaskList.RemoveAt(ix);
+                }
+            }
+
+            // 改めてタスクに加える
+            TaskList.Add(task);
+        }
+        catch (Exception)
+        {
+        }
+    }
+
+    public void Stop()
+    {
+
+        Task.WaitAll(TaskList.ToArray());
+        try
+        {
+            if (update != null)
+            {
+                // フォームを閉じた時も解放。
+                update.Stop();
+                update = null;
+            }
+            if (driver != null)
+            {
+                driver.Close();
+                driver.Quit();
+            }
+        }
+        catch (Exception)
+        {
+        }
+    }
+}
---
+++ win-hidemaru-app/hm_textsynchrome/old_cppcli_ver/HmTextSynChrome.src/HmTextSynChrome/hidemaruexe_export.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <tchar.h>
+
+#include "hidemaruexe_export.h"
+
+#pragma comment(lib, "version.lib")
+
+
+HMODULE CHidemaruExeExport::hHideExeHandle = NULL;
+TCHAR CHidemaruExeExport::szHidemaruFullPath[MAX_PATH] = L"";
+
+CHidemaruExeExport::PFNGetCurrentWindowHandle CHidemaruExeExport::Hidemaru_GetCurrentWindowHandle = NULL;
+CHidemaruExeExport::PFNGetTotalTextUnicode CHidemaruExeExport::Hidemaru_GetTotalTextUnicode = NULL;
+
+double CHidemaruExeExport::hm_version = 0;
+double CHidemaruExeExport::QueryFileVersion(TCHAR* path){
+	VS_FIXEDFILEINFO* v;
+	DWORD dwZero = 0;
+	UINT len;
+	DWORD sz = GetFileVersionInfoSize(path, &dwZero);
+	if (sz){
+		void* buf = new char[sz];
+		GetFileVersionInfo(path, dwZero, sz, buf);
+
+		if (VerQueryValue(buf, L"\\", (LPVOID*)&v, &len)){
+			double ret = 0;
+			ret = double(HIWORD(v->dwFileVersionMS)) * 100 +
+				double(LOWORD(v->dwFileVersionMS)) * 10 +
+				double(HIWORD(v->dwFileVersionLS)) +
+				double(LOWORD(v->dwFileVersionLS)) * 0.01;
+			delete buf;
+			return ret;
+		}
+		else{
+			delete buf;
+		}
+	}
+
+	return 0;
+}
+
+
+CHidemaruExeExport::CHidemaruExeExport() {
+
+	if (hHideExeHandle) {
+		return;
+	}
+
+	GetModuleFileName(NULL, szHidemaruFullPath, _countof(szHidemaruFullPath));
+	hm_version = QueryFileVersion(szHidemaruFullPath);
+
+	if (hm_version < 866) {
+		MessageBox(NULL, L"秀丸のバージョンが不足しています。\n秀丸エディタ v8.66以上が必要です。", L"情報", MB_ICONERROR);
+		return;
+	}
+
+	// 秀丸本体に関数があるのは 8.66以上
+	hHideExeHandle = LoadLibrary(szHidemaruFullPath);
+
+	if (hHideExeHandle) {
+		Hidemaru_GetTotalTextUnicode = (PFNGetTotalTextUnicode)GetProcAddress(hHideExeHandle, "Hidemaru_GetTotalTextUnicode");
+		return;
+	}
+}
+
+
+wstring CHidemaruExeExport::GetTotalText() {
+	HGLOBAL hGlobal = CHidemaruExeExport::Hidemaru_GetTotalTextUnicode();
+	if (hGlobal) {
+		wchar_t* pwsz = (wchar_t*)GlobalLock(hGlobal);
+		wstring text(pwsz); // コピー
+		GlobalUnlock(hGlobal);
+		GlobalFree(hGlobal); // 元のは解放
+		return text;
+	}
+	return L"";
+}
