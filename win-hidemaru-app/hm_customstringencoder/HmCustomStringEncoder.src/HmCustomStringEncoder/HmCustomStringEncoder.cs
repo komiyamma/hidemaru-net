cdf318ef3e4e12581bfe6cc73c03d8892325ef52
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-08-08 22:22:21 +0900

    特定の文字コードに変換した際、崩れてしまう文字の検知

---
+++ win-hidemaru-app/hm_customstringencoder/HmCustomStringEncoder.src/HmCustomStringEncoder/HmCustomStringEncoder.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using Hidemaru;
+
+// エンコードできない文字をUnicodeスカラ値表記(U+XXXX)に置き換えるEncoderFallback
+internal class HmEncoderScalarValueFallback : EncoderFallback
+{
+    // 置き換えられる代替文字列の最大文字数
+    public override int MaxCharCount
+    {
+        get { return 6; /* "U+XXXX".Length */ } // まぁ大丈夫だろ。
+    }
+
+    public override EncoderFallbackBuffer CreateFallbackBuffer()
+    {
+        // EncoderFallbackBufferのインスタンスを作成して返す
+        return new HmEncoderScalarValueFallbackBuffer();
+    }
+}
+
+// 代替文字列の生成を行うクラス
+internal class HmEncoderScalarValueFallbackBuffer : EncoderFallbackBuffer
+{
+    private char[] alternative;
+    private int offset;
+
+    // 代替文字列の残り文字数
+    public override int Remaining
+    {
+        get { return alternative.Length - offset; }
+    }
+
+    // エンコードできない文字が見つかった場合に呼び出されるメソッド
+    public override bool Fallback(char charUnknown, int index)
+    {
+        // エンコードできない文字をUnicodeスカラ値表記にした文字列を代替文字列とする
+        var codes = string.Format("[U+{0:x04}]", (int)charUnknown);
+
+        alternative = codes.ToCharArray();
+        var map = HmCustomStringEncoder.map;
+        if (map.ContainsKey(codes))
+        {
+            // すでに有るなら、置き換え
+            alternative = map[codes].ToCharArray();
+        }
+
+        offset = 0;
+
+        return true;
+    }
+
+    public override bool Fallback(char charUnknownHigh, char charUnknownLow, int index)
+    {
+        // エンコードできない文字をUnicodeスカラ値表記にした文字列を代替文字列とする
+        if (!Char.IsHighSurrogate(charUnknownHigh))
+        {
+            throw new ArgumentOutOfRangeException("charUnknownHigh", "上位文字がサロゲートペア上位文字の範囲外:0xD800, 0xDBFF");
+        }
+        if (!Char.IsLowSurrogate(charUnknownLow))
+        {
+            throw new ArgumentOutOfRangeException("charUnknownLow", "下位文字がサロゲートペア上位文字の範囲外:0xDC00, 0xDFFF");
+        }
+
+        var codes = String.Format("[U+{0:x04}][U+{1:x04}]", (int)charUnknownHigh, (int)charUnknownLow);
+        alternative = codes.ToCharArray();
+        var map = HmCustomStringEncoder.map;
+        if (map.ContainsKey(codes))
+        {
+            // すでに有るなら、置き換え
+            alternative = map[codes].ToCharArray();
+        }
+
+
+        offset = 0;
+
+        return true;
+    }
+
+    // 代替文字列の次の文字を取得するメソッド
+    public override char GetNextChar()
+    {
+        if (alternative.Length <= offset)
+        {
+            // MaxCharCountやRemainingプロパティの値は考慮されない(?)ようなので、
+            // 代替文字列の末尾に到達したらchar.MinValueを返す必要がある
+            return char.MinValue;
+        }
+        else
+        {
+            return alternative[offset++];
+        }
+    }
+
+    public override bool MovePrevious()
+    {
+        return false;
+    }
+
+    public override void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public class HmCustomStringEncoder
+{
+    // 順番を維持した方がよさそうなので、key, valueのリストで
+    public static Dictionary<string, string> map = new Dictionary<string, string>();
+
+
+    public static void Open()
+    {
+        map.Clear();
+    }
+
+    public static void PushFallBackPair(String orgcode, String dstcode)
+    {
+        try
+        {
+            String codes = "";
+            foreach (var ch in orgcode.ToCharArray())
+            {
+                codes += string.Format("[U+{0:x04}]", (int)ch);
+            }
+
+            if (map.ContainsKey(codes))
+            {
+                // すでに有るなら、置き換え
+                map[codes] = dstcode;
+            }
+            else
+            {
+                // System.Windows.Forms.MessageBox.Show(codes);
+                // 無いなら付け足し
+                map.Add(codes, dstcode);
+            }
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Trace.WriteLine(e.Message);
+        }
+    }
+
+    public static void Close()
+    {
+        map.Clear();
+    }
+
+    public static String ToEncode(IntPtr code, IntPtr isNormalize)
+    {
+        try
+        {
+            String text = "";
+            if ((Int32)(dynamic)Hm.Macro.Var["selecting"] == 1)
+            {
+                text = Hm.Edit.SelectedText;
+            } else
+            {
+                text = Hm.Edit.TotalText;
+            }
+
+            int lineCnt = text.Count(c => c == '\n');
+            if (lineCnt < 2) {
+                text += "\n\n";
+            }
+
+            String normalize_text = "";
+            if (isNormalize.ToInt32() == 1)
+            {
+                normalize_text = text.Normalize();
+            }
+            else
+            {
+                normalize_text = text;
+            }
+
+            text = Hm.Edit.TotalText;
+
+            // 独自に実装したフォールバックを指定してEncodingを取得
+            var encode = Encoding.GetEncoding(code.ToInt32(), new HmEncoderScalarValueFallback(), DecoderFallback.ReplacementFallback);
+
+            var bytes = encode.GetBytes(normalize_text);
+
+            // 再び文字列に戻して
+            return encode.GetString(bytes);
+        }
+        catch (Exception ex)
+        {
+            return ex.GetType() + ":" + ex.Message + "\n指定の「#ToEncodeCodePage」の値等が正しいか、よく確認してください。";
+        }
+    }
+}
---
+++ win-hidemaru-app/hm_customstringencoder/HmCustomStringEncoder.src/HmCustomStringEncoder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("HmCustomStringEncoder for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyDescription("HmCustomStringEncoder for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HmCustomStringEncoder for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyCopyright("VSCode.life")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("c54e0b31-4385-49cf-af00-e0aedb1f79ef")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
