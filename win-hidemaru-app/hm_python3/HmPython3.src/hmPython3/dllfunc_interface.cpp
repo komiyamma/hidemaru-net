65dbfc5483694a89361f5661881ebbc2ab01a520
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-09-03 06:27:58 +0900

    GetやSetの実装

--- win-hidemaru-app/hm_python3/HmPython3.src/hmPython3/dllfunc_interface.cpp
+++ win-hidemaru-app/hm_python3/HmPython3.src/hmPython3/dllfunc_interface.cpp
@@ -16,227 +16,77 @@ using namespace std;
 
 static int CreateScope() {
 
-	PythonEngine::Create();
+	// 既にエンジンが構築されているなら
+	if (PythonEngine::IsValid()) {
+		return TRUE;
+	}
 
-	return TRUE;
+	// エンジン構築。結果をそのまま返す
+	BOOL isValid = PythonEngine::Create();
+	return isValid;
 }
 
 
 
 //------------------------------------------------------------------------------------
-MACRO_DLL intHM_t GetNumVar(const TCHAR *sz_full_var_name) {
-	/*
+MACRO_DLL intHM_t GetNumVar(const TCHAR *utf16_simbol) {
 	if (CreateScope() == 0)
 	{
 		return 0;
 	}
 
-	// 絶対に２文字は必要
-	if (_tcslen(sz_full_var_name) < 2) {
-		return 0;
-	}
+	CSelfDllInfo::SetBindDllHandle();
 
-	char utf8_buffer[4096] = "";
+	intHM_t r = PythonEngine::GetNumVar(utf16_simbol);
 
-	// 最初の記号は除いた文字。チェックしても無駄なのでチェックしない。
-	const TCHAR *sz_var_name = sz_full_var_name + 1;
-
-	int ret = module->PerlEzEvalString(
-		module->engine,
-		string("${" + utf16_to_utf8(sz_var_name) + "};").data(),
-		utf8_buffer,
-		_countof(utf8_buffer)
-		);
-
-	// 何かエラーっぽい文字列が入っているぞ…
-	if (module->IsReturnBufferSeemsEvalError(utf8_buffer)) {
-		OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	}
-
-	// 成功
-	if (ret == plezNoError) {
-		// 数字→数値の変換
-		try {
-			wstring strnum = utf8_to_utf16(utf8_buffer);
-			return (intHM_t)std::stol(strnum);
-		}
-		catch (...) {
-
-		}
-	}
-	OutputDebugErrMsg();
-	// 失敗したらトレース枠に
-	OutputDebugStream(utf8_to_utf16(utf8_buffer));
-
-	// 失敗したら0
-	return 0;
-	*/
-
-	return 0;
+	return r;
 }
 
-MACRO_DLL intHM_t SetNumVar(const TCHAR *sz_full_var_name, intHM_t value) {
-	/*
+MACRO_DLL intHM_t SetNumVar(const TCHAR *utf16_simbol, intHM_t value) {
 	if (CreateScope() == 0)
 	{
 		return 0;
 	}
 
-	// 絶対に２文字は必要
-	if (_tcslen(sz_full_var_name) < 2) {
-		return 0;
-	}
+	CSelfDllInfo::SetBindDllHandle();
 
-	char utf8_buffer[4096] = "";
+	BOOL r = PythonEngine::SetNumVar(utf16_simbol, value);
 
-	// まずは関数を定義
-	module->PerlEzEvalString(
-		module->engine,
-		string("sub _____TmpSetVar { my ($simbol, $value) = @_; ${$simbol} = $value; }").data(),
-		utf8_buffer,
-		_countof(utf8_buffer)
-		);
-
-	// クリア
-	utf8_buffer[0] = NULL;
-
-	// 最初の記号は除いた文字。チェックしても無駄なのでチェックしない。
-	const TCHAR *sz_var_name = sz_full_var_name + 1;
-
-	int ret = module->PerlEzCall(
-		module->engine,
-		"_____TmpSetVar",
-		utf8_buffer,
-		_countof(utf8_buffer),
-		"si",
-		utf16_to_utf8(sz_var_name).data(),
-		value
-		);
-
-	// 何かエラーっぽい文字列が入っているぞ…
-	if (module->IsReturnBufferSeemsEvalError(utf8_buffer)) {
-		OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	}
-
-	// 成功
-	if (ret == plezNoError) {
-		return 1;
-	}
-	OutputDebugErrMsg();
-	// 失敗したらトレース枠に
-	OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	*/
-
-	// 失敗したら0
-	return 0;
+	return (intHM_t)r;
 }
 
 // 秀丸のキャッシュのため
 static wstring strvars;
-MACRO_DLL const TCHAR * GetStrVar(const TCHAR *sz_full_var_name) {
+MACRO_DLL const TCHAR * GetStrVar(const TCHAR *utf16_simbol) {
 	// クリア必須
 	strvars.clear();
 
-	/*
 	if (CreateScope() == 0)
 	{
 		return 0;
 	}
 
-	// 絶対に２文字は必要
-	if (_tcslen(sz_full_var_name) < 2) {
-		return 0;
-	}
+	CSelfDllInfo::SetBindDllHandle();
 
-	char utf8_buffer[4096] = "";
+	wstring strvars = PythonEngine::GetStrVar(utf16_simbol);
 
-	// 最初の記号は除いた文字。チェックしても無駄なのでチェックしない。
-	const TCHAR *sz_var_name = sz_full_var_name + 1;
-
-	int ret = module->PerlEzEvalString(
-		module->engine,
-		string("${" + utf16_to_utf8(sz_var_name) + "};").data(),
-		utf8_buffer,
-		_countof(utf8_buffer)
-		);
-
-	// 何かエラーっぽい文字列が入っているぞ…
-	if (module->IsReturnBufferSeemsEvalError(utf8_buffer)) {
-		OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	}
-
-	// 成功
-	if (ret == plezNoError) {
-		strvars = utf8_to_utf16(utf8_buffer);
-		return strvars.data();
-	}
-
-	OutputDebugErrMsg();
-	// 失敗したらトレース枠に
-	OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	*/
-	// 失敗したら""
-	return L"";
+	return strvars.c_str();
 }
 
-MACRO_DLL intHM_t SetStrVar(const TCHAR *sz_full_var_name, const TCHAR *value) {
+MACRO_DLL intHM_t SetStrVar(const TCHAR *utf16_simbol, const TCHAR *utf16_value) {
 	if (CreateScope() == 0)
 	{
 		return 0;
 	}
 
-	/*
+	CSelfDllInfo::SetBindDllHandle();
 
-	// 絶対に２文字は必要
-	if (_tcslen(sz_full_var_name) < 2) {
-		return 0;
-	}
+	BOOL r = PythonEngine::SetStrVar(utf16_simbol, utf16_value);
 
-	char utf8_buffer[4096] = "";
-
-	// まずは関数を定義
-	module->PerlEzEvalString(
-		module->engine,
-		string("sub _____TmpSetVar { my ($simbol, $value) = @_; ${$simbol} = $value; }").data(),
-		utf8_buffer,
-		_countof(utf8_buffer)
-		);
-
-	// クリア
-	utf8_buffer[0] = NULL;
-
-	// 最初の記号は除いた文字。チェックしても無駄なのでチェックしない。
-	const TCHAR *sz_var_name = sz_full_var_name + 1;
-
-	int ret = module->PerlEzCall(
-		module->engine,
-		"_____TmpSetVar",
-		utf8_buffer,
-		_countof(utf8_buffer),
-		"ss",
-		utf16_to_utf8(sz_var_name).data(),
-		utf16_to_utf8(value).data()
-		);
-
-	// 何かエラーっぽい文字列が入っているぞ…
-	if (module->IsReturnBufferSeemsEvalError(utf8_buffer)) {
-		OutputDebugStream(utf8_to_utf16(utf8_buffer));
-	}
-
-	// 成功
-	if (ret == plezNoError) {
-		return 1;
-	}
-	OutputDebugErrMsg();
-	// 失敗したらトレース枠に
-	OutputDebugStream(utf8_to_utf16(utf8_buffer));
-
-	*/
-	// 失敗したら0
-	return 0;
+	return (intHM_t)r;
 }
 
-MACRO_DLL intHM_t DoString(const TCHAR *szexpression) {
+MACRO_DLL intHM_t DoString(const TCHAR *utf16_expression) {
 	if (CreateScope() == 0)
 	{
 		return 0;
@@ -259,9 +109,7 @@ MACRO_DLL intHM_t DoString(const TCHAR *szexpression) {
 		MessageBox(NULL, L"引数の型が異なります。\ndllfuncではなく、dllfuncw文を利用してください。", L"引数の型が異なります", MB_ICONERROR);
 	}
 
-	string utf8_expression = utf16_to_utf8(szexpression);
-
-	PythonEngine::DoString(utf8_expression);
+	PythonEngine::DoString(utf16_expression);
 
 	// 失敗したら0
 	return 0;
@@ -269,32 +117,14 @@ MACRO_DLL intHM_t DoString(const TCHAR *szexpression) {
 
 
 MACRO_DLL intHM_t DestroyScope() {
-	/*
-	if (module) {
-
-		// 最後に「DestroyScope」というグローバル関数がPerl内に定義されていれば、それを実行する。
-		char utf8_buffer[4096] = "";
-		int ret = module->PerlEzCall(
-			module->engine,
-			"DestroyScope",
-			utf8_buffer,
-			_countof(utf8_buffer),
-			""
-			);
-
-		// 何かエラーっぽい文字列が入っているぞ…
-		if (module->IsReturnBufferSeemsEvalError(utf8_buffer)) {
-			OutputDebugStream(utf8_to_utf16(utf8_buffer));
-			OutputDebugStream(L"error in DestroyScope");
-		}
-
-		delete module;
-		module = NULL;
-
+	if (CreateScope() == 0)
+	{
+		return 0;
 	}
 
-	TestDynamicVar.Clear();
-	*/
+	// DoStringされる度にdllのBindの在り方を確認更新する。
+	CSelfDllInfo::SetBindDllHandle();
+
 	PythonEngine::Destroy();
 
 	return TRUE;
