1e67f0e8401d9e666490e8b2b4b240a613c18b48
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-09-03 22:43:02 +0900

    コメントの追加

--- win-hidemaru-app/hm_python3/hmPython.py
+++ win-hidemaru-app/hm_python3/hmPython.py
@@ -1,32 +1,46 @@
 import hidemaru
 
 
-class _TEdit:
 
+class _TEdit:
+    """
+    秀丸で編集中のドキュメント関連のクラス
+    """
+    
+    #--------------------------------------------------
     def __GetTotalText(self):
         return hidemaru.edit.get_totaltext()
 
     def __SetTotalText(self, text):
         return hidemaru.edit.set_totaltext(text)
 
+    # 編集中のテキスト全体
     TotalText = property(__GetTotalText, __SetTotalText)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     def __GetSelectedText(self):
         return hidemaru.edit.get_selectedtext()
 
     def __SetSelectedText(self, text):
         return hidemaru.edit.set_selectedtext(text)
 
+    # 選択中のテキスト。通常選択もしくは行選択のみ（矩形選択は対象としない）
     SelectedText = property(__GetSelectedText, __SetSelectedText)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     def __GetLineText(self):
         return hidemaru.edit.get_linetext()
 
     def __SetLineText(self, text):
         return hidemaru.edit.set_linetext(text)
 
+    # カーソルがある行のテキスト
     LineText = property(__GetLineText, __SetLineText)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     class _TCursorPos:
 
         def __init__(self, lineno, column):
@@ -37,8 +51,11 @@ class _TEdit:
         lineno, column = hidemaru.edit.get_cursorpos()
         return _TEdit._TCursorPos(lineno, column)
 
+    # カーソルの位置情報。linenoとcolumn（マクロのlinenoとcolumnと同じ値）
     CursorPos = property(__GetCursorPos)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     class _TMousePos:
 
         def __init__(self, lineno, column, x, y):
@@ -51,20 +68,35 @@ class _TEdit:
         lineno, column, x, y = hidemaru.edit.get_mousepos()
         return _TEdit._TMousePos(lineno, column, x, y)
 
+    # マウスの位置に対応するカーソルの情報。
+    # linenoとcolumn（マクロのlinenoとcolumnと同じ値）  xとyは、win32 apiのマウス位置情報と同じ
     MousePos = property(__GetMousePos)
+    #--------------------------------------------------
+
 
 
 class _TMacro:
-
+    """
+    秀丸マクロ関連のクラス
+    """
+    #--------------------------------------------------
     class _TVar:
+        """
+        秀丸マクロ関連のうち、マクロシンボル（マクロ変数）を扱うクラス
+        """
 
        def __getitem__(self, varname):
            return hidemaru.macro.get_var(varname)
 
        def __setitem__(self, varname, value):
            return hidemaru.macro.set_var(varname, value)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     class _TResult:
+        """
+        秀丸マクロ関連のうち、マクロ実行結果情報を扱うクラス
+        """
 
         def __init__(self, Result, Message, ErrorMsg):
             self.Result = Result
@@ -73,31 +105,48 @@ class _TMacro:
                 self.Error = None
             else:
                 self.Error = RuntimeError(ErrorMsg)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     def __init__(self):
         self.Var  = _TMacro._TVar()
+    #--------------------------------------------------
 
+    #--------------------------------------------------
+    # マクロの実行
     def Eval(self, expression_text):
         res, msg, errmsg = hidemaru.macro.do_eval(expression_text)
         ret = _TMacro._TResult(res, msg, errmsg)
         return ret
-
+    #--------------------------------------------------
 
 
 
 class _THidemaru:
+    """
+    特定のカテゴリに所属しないようなもの
+    """
+    #--------------------------------------------------
     def __init__(self):
         self.Edit  = _TEdit()
         self.Macro = _TMacro()
+    #--------------------------------------------------
 
-    def debuginfo(self, text):
-        return hidemaru.debug_info(text)
+    #--------------------------------------------------
+    # デバッグモニターに出力
+    def debuginfo(self, obj):
+        return hidemaru.debug_info(obj)
+    #--------------------------------------------------
 
+    #--------------------------------------------------
     def __GetVersion(self):
         return hidemaru.get_version()
 
+    # 秀丸のバージョンを 866.05 のような形で取得。
+    # 「β5」なら少数部分が「05」のような形。
+    # 正式版だと、866.99 のように「99」となる。
     version = property(__GetVersion)
-
+    #--------------------------------------------------
 
 
 
