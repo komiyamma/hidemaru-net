022416c3719fe46158aff7d216e5c1aead043a07
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-09-21 00:41:09 +0900

    非選択になった時にクリアしていなかったので、クリア

--- win-hidemaru-app/hm_selctedtextautohighlighter.src/HmSelctedTextAutoHighlighter.src/HmSelctedTextAutoHighlighter/HmSelctedTextAutoHighlighter.cs
+++ win-hidemaru-app/hm_selctedtextautohighlighter.src/HmSelctedTextAutoHighlighter.src/HmSelctedTextAutoHighlighter/HmSelctedTextAutoHighlighter.cs
@@ -3,40 +3,40 @@ using System;
 
 public class HmSelctedTextAutoHighlighter
 {
-	public static IntPtr Start()
-	{
-		IntPtr result;
-		try
-		{
-			if (HmSelctedTextAutoHighlighterForm.form == null)
-			{
-				HmSelctedTextAutoHighlighterForm.form = new HmSelctedTextAutoHighlighterForm();
-			}
-			result = (IntPtr)1;
-		}
-		catch (Exception)
-		{
-			result = (IntPtr)0;
-		}
-		return result;
-	}
+    public static IntPtr Start()
+    {
+        IntPtr result;
+        try
+        {
+            if (HmSelctedTextAutoHighlighterForm.form == null)
+            {
+                HmSelctedTextAutoHighlighterForm.form = new HmSelctedTextAutoHighlighterForm();
+            }
+            result = (IntPtr)1;
+        }
+        catch (Exception)
+        {
+            result = (IntPtr)0;
+        }
+        return result;
+    }
 
-	public static IntPtr OnDetachMethod()
-	{
-		IntPtr result;
-		try
-		{
-			if (HmSelctedTextAutoHighlighterForm.form != null)
-			{
-				HmSelctedTextAutoHighlighterForm.form.Stop();
-				HmSelctedTextAutoHighlighterForm.form = null;
-			}
-			result = (IntPtr)1;
-		}
-		catch (Exception)
-		{
-			result = (IntPtr)0;
-		}
-		return result;
-	}
+    public static IntPtr OnDetachMethod()
+    {
+        IntPtr result;
+        try
+        {
+            if (HmSelctedTextAutoHighlighterForm.form != null)
+            {
+                HmSelctedTextAutoHighlighterForm.form.Stop();
+                HmSelctedTextAutoHighlighterForm.form = null;
+            }
+            result = (IntPtr)1;
+        }
+        catch (Exception)
+        {
+            result = (IntPtr)0;
+        }
+        return result;
+    }
 }
--- win-hidemaru-app/hm_selctedtextautohighlighter.src/HmSelctedTextAutoHighlighter.src/HmSelctedTextAutoHighlighter/HmSelctedTextAutoHighlighterForm.cs
+++ win-hidemaru-app/hm_selctedtextautohighlighter.src/HmSelctedTextAutoHighlighter.src/HmSelctedTextAutoHighlighter/HmSelctedTextAutoHighlighterForm.cs
@@ -5,81 +5,108 @@ using System.Windows.Forms;
 
 public class HmSelctedTextAutoHighlighterForm : Form
 {
-	public static HmSelctedTextAutoHighlighterForm form;
-	private Timer timer;
-	private int KeepMonitoringMilliSeconds = 1000;
-	private List<string> buffer;
-	private string strLastText = "";
+    public static HmSelctedTextAutoHighlighterForm form;
+    private Timer timer;
+    private int KeepMonitoringMilliSeconds = 1000;
+    private List<string> buffer;
+    private string strLastText = "";
 
-	public HmSelctedTextAutoHighlighterForm()
-	{
-		buffer = new List<string>();
+    public HmSelctedTextAutoHighlighterForm()
+    {
+        buffer = new List<string>();
 
-		timer = new Timer();
-		timer.Interval = 300;
-		timer.Enabled = true;
-		timer.Start();
-		timer.Tick += timer_Tick;
-	}
+        timer = new Timer();
+        timer.Interval = 300;
+        timer.Tick += timer_Tick;
+        timer.Enabled = true;
+        timer.Start();
+    }
 
-	private void UpdateHidemaruMarker(string text)
-	{
-		if (!Hm.Macro.IsExecuting)
-		{
-			var ret = Hm.Macro.Exec.Eval(
-            @"
-                setcompatiblemode 0x20000;
-                deletecolormarker ""HmSelctedTextTmpColorMarker"", 33001;
-                $text = gettext(seltopx, seltopy, selendx, selendy, 1);
-                setsearch $text, 0x00000002;
-                colormarkerallfound 0x333333,0x8CE6F0,-1,1,33001,""HmSelctedTextTmpColorMarker"";
-                endmacro ""Complete"";
-            ");
-
-            if (ret.Message == "Complete")
+    private bool TryUpdateHidemaruMarker(string text)
+    {
+        if (!Hm.Macro.IsExecuting)
+        {
+            if (text.Length == 0)
             {
-                strLastText = text;
+
+                var ret = Hm.Macro.Exec.Eval(
+                @"
+                    deletecolormarker ""HmSelctedTextTmpColorMarker"", 33001;
+                    endmacro ""CompleteClear"";
+                ");
+
+                if (ret.Message == "CompleteClear")
+                {
+                    return true;
+                }
+
             }
+            else { 
+
+                var ret = Hm.Macro.Exec.Eval(
+                @"
+                    setcompatiblemode 0x20000;
+                    deletecolormarker ""HmSelctedTextTmpColorMarker"", 33001;
+                    $text = gettext(seltopx, seltopy, selendx, selendy, 1);
+                    setsearch $text, 0x00000002;
+                    colormarkerallfound 0x333333,0x8CE6F0,-1,1,33001,""HmSelctedTextTmpColorMarker"";
+                    endmacro ""CompleteHighlight"";
+                ");
+
+                if (ret.Message == "CompleteHighlight")
+                {
+                    return true;
+                }
+            }
+
         }
-	}
+
+        return false;
+    }
 
 
 
-	private void timer_Tick(object sender, EventArgs e)
-	{
-		try
-		{
-			Timer timer = sender as Timer;
+    private void timer_Tick(object sender, EventArgs e)
+    {
+        try
+        {
+            Timer timer = sender as Timer;
 
             var text = Hm.Edit.SelectedText;
+
+            // 長すぎる文字列をハイライトとか変。やめ
             if (text.Length > 100)
             {
                 return;
             }
+
             buffer.Add(Hm.Edit.SelectedText);
 
-			if (buffer.Count > KeepMonitoringMilliSeconds / timer.Interval)
-			{
-				buffer.RemoveAt(0);
+            if (buffer.Count > KeepMonitoringMilliSeconds / timer.Interval)
+            {
+                buffer.RemoveAt(0);
 
                 if (text != strLastText)
                 {
                     if (buffer.TrueForAll(tx => tx == text))
                     {
-                        UpdateHidemaruMarker(text);
+                        if (TryUpdateHidemaruMarker(text))
+                        {
+                            strLastText = text;
+                        }
                     }
                 }
-			}
-		}
-		catch (Exception)
-		{
-		}
-	}
+            }
+        }
+        catch (Exception)
+        {
+        }
+    }
 
-	public void Stop()
-	{
-		timer.Enabled = false;
-		timer.Stop();
-		timer = null;
-	}
+    public void Stop()
+    {
+        timer.Enabled = false;
+        timer.Stop();
+        timer = null;
+    }
 }
