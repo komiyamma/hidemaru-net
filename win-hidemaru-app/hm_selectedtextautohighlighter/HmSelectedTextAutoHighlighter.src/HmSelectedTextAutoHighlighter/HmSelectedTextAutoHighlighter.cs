53f60bb691b1197f3efe8f4a3c5fb21e8eb3d27d
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-09-21 15:56:14 +0900

    最低限の仕立てなおし。名前がまちがっていた問題など

---
+++ win-hidemaru-app/hm_selectedtextautohighlighter/HmSelectedTextAutoHighlighter.src/HmSelectedTextAutoHighlighter/HmSelectedTextAutoHighlighter.cs
@@ -0,0 +1,40 @@
+using System;
+
+
+public class HmSelectedTextAutoHighlighter
+{
+    public static IntPtr Start()
+    {
+        try
+        {
+            if (HmSelectedTextAutoHighlighterForm.form == null)
+            {
+                HmSelectedTextAutoHighlighterForm.form = new HmSelectedTextAutoHighlighterForm();
+            }
+            return (IntPtr)1;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Trace.WriteLine(ex);
+            return (IntPtr)0;
+        }
+    }
+
+    public static IntPtr OnDetachMethod()
+    {
+        try
+        {
+            if (HmSelectedTextAutoHighlighterForm.form != null)
+            {
+                HmSelectedTextAutoHighlighterForm.form.Stop();
+                HmSelectedTextAutoHighlighterForm.form = null;
+            }
+            return (IntPtr)1;
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Trace.WriteLine(ex);
+            return (IntPtr)0;
+        }
+    }
+}
