6fed49eadd9a8ed308181d848d975cd0bb686446
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-10-22 14:34:45 +0900

    Java9は64bit版しか出ないとわかったので、対応しないこととした。

--- win-hidemaru-app/hm_javavm/hmJavaVM/hidemaru/Hm.java
+++ win-hidemaru-app/hm_javavm/hmJavaVM/hidemaru/Hm.java
@@ -8,99 +8,7 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.HashMap;
 
-/*
-import java.io.File;
-import java.nio.file.Path; 
-import java.nio.file.Paths; 
-import java.nio.file.Files; 
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Objects;
-import java.util.Collections;
-import java.lang.IllegalArgumentException;
-import java.net.MalformedURLException;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.HashMap;
-*/
-
 public class Hm {
-
-	/*
-    private static class SpclClassLoader extends URLClassLoader
-    {
-        static
-        {
-            ClassLoader.registerAsParallelCapable();
-        }
-
-        private final Set<Path> userLibPaths = new CopyOnWriteArraySet<>();
-
-        private SpclClassLoader()
-        {
-            super(new URL[0]);
-        }
-
-        @Override
-        protected void addURL(URL url)
-        {
-            super.addURL(url);
-        }
-
-        protected void addLibPath(String newpath)
-        {
-            userLibPaths.add(Paths.get(newpath).toAbsolutePath());
-        }
-
-        @Override
-        protected String findLibrary(String libname)
-        {
-            String nativeName = System.mapLibraryName(libname);
-            return userLibPaths.stream().map(tpath -> tpath.resolve(nativeName)).filter(Files::exists).map(Path::toString).findFirst().orElse(super.findLibrary(libname));            }
-    }
-    private final static SpclClassLoader ucl = new SpclClassLoader();
-	*/
-
-    /**
-     * Adds a jar file or directory to the classpath. From Utils4J.
-     *
-     * @param newpaths JAR filename(s) or directory(s) to add
-     * @return URLClassLoader after newpaths added if newpaths != null
-     */
-	 /*
-    public static ClassLoader addToClasspath(String... newpaths)
-    {
-        if (newpaths != null)
-            try
-            {
-                for (String newpath : newpaths)
-                    if (newpath != null && !newpath.trim().isEmpty())
-                        ucl.addURL(Paths.get(newpath.trim()).toUri().toURL());
-            }
-            catch (IllegalArgumentException | MalformedURLException e)
-            {
-					Hm.debugInfo("エラーが出たよ");
-                RuntimeException re = new RuntimeException(e);
-                re.setStackTrace(e.getStackTrace());
-                throw re;
-            }
-        return ucl;
-    }
-	*/
-    /**
-     * Adds to library path in ClassLoader returned by addToClassPath
-     *
-     * @param newpaths Path(s) to directory(s) holding OS library files
-     */
-	 /*
-    public static void addToLibraryPath(String... newpaths)
-    {
-        for (String newpath : Objects.requireNonNull(newpaths))
-            ucl.addLibPath(newpath);
-    }
-	*/
 
 	// ライブラリをロード
 	static { 
@@ -404,17 +312,9 @@ public class Hm {
 	 * そのパスのlibサブディレクトリの追加。
 	 */
 	protected static void _AddClassPath(String[] _dummy){
-		// Thread.currentThread().setContextClassLoader(addToClasspath());
 
 		String currentmacrodirectory = (String)Hm.Macro.getVar("currentmacrodirectory");
 		_AddClassPath(currentmacrodirectory);
-		// addToClasspath(currentmacrodirectory);
-		// Hm.debugInfo("path:" + currentmacrodirectory);
-		/*
-		String currentmacrofilename = (String)Hm.Macro.getVar("currentmacrofilename");
-		String path2 = _GetFiNamePrefix( currentmacrofilename ) + ".jar";
-		_AddClassPath(path2);
-		*/
 
         // ディレクトリ配下を探索
         File[] filelist = new File(currentmacrodirectory).listFiles();
@@ -424,8 +324,6 @@ public class Hm {
 				// 見つかったファイルがJARの場合は追加
 				if (suffix.equalsIgnoreCase("jar")) {
 					_AddClassPath(currentmacrodirectory + "\\" + file.getName());
-					// addToClasspath(currentmacrodirectory + "\\" + file.getName());
-					// Hm.debugInfo("jar:" + currentmacrodirectory + "\\" + file.getName());
 				}
 			}
         }
