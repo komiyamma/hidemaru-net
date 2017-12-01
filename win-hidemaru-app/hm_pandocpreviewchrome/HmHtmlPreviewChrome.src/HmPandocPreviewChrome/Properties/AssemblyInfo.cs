c2fa170e958346914f13ddf12e0f15341d21786f
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-12-01 21:44:08 +0900

    Chrome側を閉じた時に、内部的にクローズしていなかった。

--- win-hidemaru-app/hm_pandocpreviewchrome/HmHtmlPreviewChrome.src/HmPandocPreviewChrome/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_pandocpreviewchrome/HmHtmlPreviewChrome.src/HmPandocPreviewChrome/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("HmTextSynChrome for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyTitle("HmPandocPreviewChrome for .NET 4.5 ANYCPU 版")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HmTextSynChrome for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyProduct("HmPandocPreviewChrome for .NET 4.5 ANYCPU 版")]
 [assembly: AssemblyCopyright("Copyright © 2017 VSCode.life")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("59d0d7b5-ad7c-4d3a-9df1-713172f2fe41")]
+[assembly: Guid("f1876f3b-7bbe-44ad-a803-0f12ea03217d")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.2")]
-[assembly: AssemblyFileVersion("1.2.0.2")]
+[assembly: AssemblyVersion("1.3.0.1")]
+[assembly: AssemblyFileVersion("1.3.0.1")]
