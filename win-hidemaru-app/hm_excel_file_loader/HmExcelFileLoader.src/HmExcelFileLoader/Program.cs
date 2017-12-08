a7b97f0bf641e45d5089d81700c050e9cb48706d
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2017-12-08 20:58:44 +0900

    ライセンスの整理

--- win-hidemaru-app/hm_excel_file_loader/HmExcelFileLoader.src/HmExcelFileLoader/Program.cs
+++ win-hidemaru-app/hm_excel_file_loader/HmExcelFileLoader.src/HmExcelFileLoader/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 2017 Akitsugu Komiyama
+ * under the MIT License
+ */
+
+using System;
 using System.IO;
 using NPOI.HSSF.UserModel;
 using NPOI.XSSF.UserModel;
