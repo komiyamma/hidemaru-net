90cd5f004f304a5a48f9d176fe8e7e17f55e8ae4
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2017-12-14 01:18:57 +0900

    数式が入った.mdを、pngにするためのフィルターの一部。MathJaxの文字列をsvgに変換し、それを2倍の大きさのpngへと変換。

---
+++ win-hidemaru-app/hm_pandocmathfilter/HmMathjaxToSvg.node.js
@@ -0,0 +1,50 @@
+﻿/*
+ * Copyright (c) 2017 Akitsugu Komiyama
+ * under the MIT License
+ *
+ * 要 mathjax-node : (npm install mathjax-node)
+ * 要 magick.exe : (ImageMagick for Windows)
+ */
+
+
+// ファイル出力用
+const fs = require("fs"); 
+const mjAPI = require("mathjax-node");
+
+
+mjAPI.config(
+    {
+        MathJax:
+        {
+            // traditional MathJax configuration
+        }
+    }
+);
+mjAPI.start();
+
+
+var yourMath = fs.readFileSync("math.txt", "utf-8");
+
+const execSync = require('child_process').exec;
+
+mjAPI.typeset
+(
+    {
+        math: yourMath,
+        format: "TeX", // "inline-TeX", "MathML"
+        svg: true,
+    },
+    function (data) {
+        if (!data.errors) {
+            // console.log(data.svg);
+
+            // ファイル出力
+            fs.writeFileSync("math.svg", data.svg);
+
+            // 通常ブラウザで表示する際の2倍ぐらいで出る
+            const result = execSync("magick -density 200 math.svg math.png")
+
+        }
+    }
+);
+
