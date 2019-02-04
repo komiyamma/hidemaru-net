62889505aa19865aa277c0e6452458b4db76dd61
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2019-02-05 04:32:38 +0900

    VS2017対応等

--- win-hidemaru-app/hm_chromebrowsermode/HmChromeBrowserMode.src/HmChromeBrowserMode/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_chromebrowsermode/HmChromeBrowserMode.src/HmChromeBrowserMode/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 #else
 [assembly: AssemblyProduct("HmChromeBrowserMode for .NET 4.5.2 x86 版")]
 #endif
-[assembly: AssemblyCopyright("Copyright © 2017-2018 Akitsugu Komiyama")]
+[assembly: AssemblyCopyright("Copyright © 2017-2019 Akitsugu Komiyama")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
