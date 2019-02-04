cfdb114d4180f18f672478aa04918fd8abdc6e04
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2019-02-05 01:36:44 +0900

    hmDebugMonitorをVS2013⇒VS2017へ

--- win-hidemaru-app/hm_ecmascript6/ClearWork/ClearScript.5.5.4/ClearScriptConsole/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_ecmascript6/ClearWork/ClearScript.5.5.4/ClearScriptConsole/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 
@@ -9,9 +9,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ClearScript Test Console")]
-[assembly: AssemblyProduct("ClearScript")]
-[assembly: AssemblyCopyright("(c) Microsoft Corporation")]
+[assembly: AssemblyTitle("iv8")]
+[assembly: AssemblyProduct("ClearScript V8 Console for .NET 4.5 ANYCPU 版")]
+[assembly: AssemblyCopyright("© 2019 Microsoft Corporation, Google Corporation")]
 
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("5.5.4.0")]
