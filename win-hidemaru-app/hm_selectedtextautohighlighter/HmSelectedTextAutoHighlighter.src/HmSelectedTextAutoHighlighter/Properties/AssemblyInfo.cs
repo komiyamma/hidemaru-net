e42e18f9a8886658e41cd9c79eefa9033468a3dd
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2019-02-10 15:25:44 +0900

    VS2017への対応など

--- win-hidemaru-app/hm_selectedtextautohighlighter/HmSelectedTextAutoHighlighter.src/HmSelectedTextAutoHighlighter/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_selectedtextautohighlighter/HmSelectedTextAutoHighlighter.src/HmSelectedTextAutoHighlighter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HmSelectedTextAutoHighlighter for .NET 4.5 ANYCPU")]
-[assembly: AssemblyCopyright("Copyright (c) 2017 Akitsugu Komiyama")]
+[assembly: AssemblyCopyright("Copyright (c) 2017-2019 Akitsugu Komiyama")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
