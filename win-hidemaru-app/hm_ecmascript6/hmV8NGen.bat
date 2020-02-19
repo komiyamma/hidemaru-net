187a326bc2439331e47821ca52a1efc6db59f665
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2020-02-20 00:27:25 +0900

    NGen for hmV8

---
+++ win-hidemaru-app/hm_ecmascript6/hmV8NGen.bat
@@ -0,0 +1,9 @@
+cd %windir%\Microsoft.NET\Framework\v4.0.30319
+ngen install "C:\usr\hidemaru\hmV8.dll"
+ngen install "C:\usr\hidemaru\ClearScript.dll"
+ngen install "C:\usr\hidemaru\ClearScriptV8-32.dll"
+
+cd %windir%\Microsoft.NET\Framework64\v4.0.30319
+ngen install "C:\usr\hidemaru\hmV8.dll"
+ngen install "C:\usr\hidemaru\ClearScript.dll"
+ngen install "C:\usr\hidemaru\ClearScriptV8-64.dll"
