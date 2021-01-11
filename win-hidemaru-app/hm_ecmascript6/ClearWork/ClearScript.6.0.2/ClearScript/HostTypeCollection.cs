64b8d154a3d65c86f8e0b5396801415bda591554
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-12 04:22:27 +0900

    hmV8 を ClearScriptやV8エンジン部分をアップデートする準備

--- win-hidemaru-app/hm_ecmascript6/ClearWork/ClearScript.6.0.2/ClearScript/HostTypeCollection.cs
+++ win-hidemaru-app/hm_ecmascript6/ClearWork/ClearScript.6.0.2/ClearScript/HostTypeCollection.cs
@@ -98,11 +98,21 @@ namespace Microsoft.ClearScript
         /// Adds types from an assembly to a host type collection. The assembly is specified by name.
         /// </summary>
         /// <param name="assemblyName">The name of the assembly that contains the types to add.</param>
+        /// 
         public void AddAssembly(string assemblyName)
         {
-            MiscHelpers.VerifyNonBlankArgument(assemblyName, "assemblyName", "Invalid assembly name");
-            AddAssembly(Assembly.Load(AssemblyTable.GetFullAssemblyName(assemblyName)));
+            // ★★ そもそもターゲットとなっている場所に存在する(多分フルパスで指定している、など)
+            if (System.IO.File.Exists(assemblyName))
+            {
+                AddAssembly(Assembly.LoadFile(assemblyName));
+            }
+            else
+            {
+                MiscHelpers.VerifyNonBlankArgument(assemblyName, "assemblyName", "Invalid assembly name");
+                AddAssembly(Assembly.Load(AssemblyTable.GetFullAssemblyName(assemblyName)));
+            }
         }
+
 
         /// <summary>
         /// Adds selected types from an assembly to a host type collection.
