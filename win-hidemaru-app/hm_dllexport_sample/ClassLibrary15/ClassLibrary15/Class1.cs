f13d3fedd2887ff8e7b87c39def5ec23e9fb4f5f
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-17 15:43:00 +0900

    後日秀丸マクロからC#中間アセンブリをhm.NETやCOMなしで直接普通に呼び出せるようにする方法を解説するためのサンプル。

---
+++ win-hidemaru-app/hm_dllexport_sample/ClassLibrary15/ClassLibrary15/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+
+[SecurityPermission(SecurityAction.Demand, UnmanagedCode = true)]
+internal sealed class StaticWStrPtrHandle : SafeHandle
+{
+    private StaticWStrPtrHandle() : base(IntPtr.Zero, true) { }
+    public StaticWStrPtrHandle(String managedString) : base(IntPtr.Zero, true)
+    {
+        handle = Marshal.StringToHGlobalUni(managedString);
+    }
+
+    public static implicit operator StaticWStrPtrHandle(String managedString)
+    {
+        return new StaticWStrPtrHandle(managedString);
+    }
+
+    public static implicit operator String(StaticWStrPtrHandle managedString)
+    {
+        return managedString.ToString();
+    }
+
+    [ReliabilityContract(Consistency.WillNotCorruptState, Cer.MayFail)]
+    protected override bool ReleaseHandle()
+    {
+        try
+        {
+            Marshal.FreeHGlobal(handle);
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Trace.WriteLine("文字列解放エラー:\n" + ex.Message);
+            return false;
+        }
+
+        return true;
+    }
+
+    public override bool IsInvalid
+    {
+        get { return (IntPtr.Zero == handle); }
+    }
+
+    public override string ToString()
+    {
+        if (this.IsInvalid)
+        {
+            return String.Empty;
+        }
+        else
+        {
+            return Marshal.PtrToStringUni(handle);
+        }
+    }
+
+}
+
+
+
+namespace ClassLibrary15
+{
+    public class Class1
+    {
+        [DllExport]
+        public static IntPtr Count(IntPtr a, IntPtr b)
+        {
+            return a;
+        }
+
+        [DllExport]
+        public static IntPtr Through(IntPtr abc)
+        {
+            String str = Marshal.PtrToStringAuto(abc);
+            return (IntPtr)str.Length;
+        }
+
+        static StaticWStrPtrHandle hWStrPtr = "テストテスト";
+        [DllExport]
+        public static IntPtr GetString() 
+        {
+            hWStrPtr = "abc";
+            return hWStrPtr.DangerousGetHandle();
+        }
+
+
+
+    }
+}
+
---
+++ win-hidemaru-app/hm_dllexport_sample/ClassLibrary15/ClassLibrary15/Class2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+
+
+
+
+namespace Pattern2
+{
+
+    public class Pattern2
+    {
+
+        [DllExport]
+        public static IntPtr MyFunc1(IntPtr a, IntPtr b)
+        {
+            return a;
+        }
+
+        [DllExport]
+        public unsafe static IntPtr MyFunc2(IntPtr obj)
+        {
+            System.Diagnostics.Trace.WriteLine(obj.ToString());
+            System.Diagnostics.Trace.WriteLine(obj.GetType().ToString());
+
+            // 文字列を指す生ポインタの内容をマネージド文字列へとコピー
+            // String managed_str = new string(p_str);
+
+            // 文字列の長さを返す。
+            return (IntPtr)1; //(IntPtr)managed_str.Length;
+        }
+
+        [DllExport]
+        public unsafe static IntPtr MyFunc3(char* p_wstr)
+        {
+            String wstr = new String(p_wstr);
+            System.Diagnostics.Trace.WriteLine(wstr);
+            // 文字列の長さを返す。
+            return (IntPtr)(wstr.Length);
+        }
+
+
+        // [return: MarshalAs(UnmanagedType.LPWStr)]
+        static String rtnBufferOfGetString = "";
+        [DllExport]
+        public unsafe static char* GetString()
+        {
+            rtnBufferOfGetString = "333";
+            // マネージドな文字列の内容を指す生ポインタを返す。
+            // 生ポインタを指す内容が関数の終了とともに消えないことを保証するためには、rtnStringBuffer はメソッドスコープではなくクラススコープであるべきだ。
+            // 生ポインタが指す文字列の中身は秀丸はいつまでも要求するわけではなく、このメソッド呼び出し時に秀丸マクロ層で変数で受け止めていれば、
+            // 秀丸側は「文字列をコピーする」。
+            // よって、「メソッドが呼び出された瞬間だけ生ポインタは文字列をさしていればよく」いつまでも文字列を保持する必要はない。
+            fixed (char* p_str = rtnBufferOfGetString) { return p_str; }
+            // return strBufferOfGetString;
+        }
+
+
+
+    }
+}
