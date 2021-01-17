5cc048b825946f33c740c99ff28871bb9d795605
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-17 15:53:43 +0900

    hm.NETで主に利用されるであろう .NET Framework 4.8 と 今後の主流である.NET5とのコンパチブルプロジェクトの考え方。後日解説のためソース。覚書相当

---
+++ win-hidemaru-app/hm_dotnet_compatible_sample/ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+public class ABC
+{
+    public static void Main(String[] args)
+    {
+        Form f = new Form();
+        f.ShowDialog();
+    }
+}
