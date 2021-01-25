0bbd0d88be4f613df8107d92465e9a188c3652ea
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-01-25 16:32:36 +0900

    相対ではなく、絶対で

--- win-hidemaru-app/hm_dotnet_compatible/sample/Class1.cs
+++ win-hidemaru-app/hm_dotnet_compatible/sample/Class1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
-
+using System.Windows.Forms;
 namespace sample
 {
     public class Class1
     {
+        static void Main(String[] args)
+        {
+            Form f = new Form();
+            f.ShowDialog();
+        }
     }
 }
