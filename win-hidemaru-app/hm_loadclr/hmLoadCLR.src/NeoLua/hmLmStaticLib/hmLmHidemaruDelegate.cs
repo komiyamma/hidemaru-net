47dab73f637f747c308248fa460cbc7bde653d47
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-02-26 17:38:19 +0900

    hmLNにOutput系の機能追加

--- win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmHidemaruDelegate.cs
+++ win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmHidemaruDelegate.cs
@@ -14,6 +14,7 @@ public sealed partial class hmLmDynamicLib
 {
     public sealed partial class Hidemaru
     {
+        delegate IntPtr TGetCurrentWindowHandle();
         delegate IntPtr TGetTotalTextUnicode();
         delegate IntPtr TGetLineTextUnicode(int nLineNo);
         delegate IntPtr TGetSelectedTextUnicode();
@@ -36,6 +37,8 @@ public sealed partial class hmLmDynamicLib
         delegate int TAnalyzeEncoding([MarshalAs(UnmanagedType.LPWStr)] String pwszFileName, IntPtr lParam1, IntPtr lParam2);
         delegate IntPtr TLoadFileUnicode([MarshalAs(UnmanagedType.LPWStr)] String pwszFileName, int nEncode, ref int pcwchOut, IntPtr lParam1, IntPtr lParam2);
 
+        // 秀丸本体から出ている関数群
+        static TGetCurrentWindowHandle pGetCurrentWindowHandle;
         static TGetTotalTextUnicode pGetTotalTextUnicode;
         static TGetLineTextUnicode pGetLineTextUnicode;
         static TGetSelectedTextUnicode pGetSelectedTextUnicode;
@@ -45,6 +48,19 @@ public sealed partial class hmLmDynamicLib
         static TCheckQueueStatus pCheckQueueStatus;
         static TAnalyzeEncoding pAnalyzeEncoding;
         static TLoadFileUnicode pLoadFileUnicode;
+
+        // OutputPaneから出ている関数群
+        delegate int TOutputPane_Output(IntPtr hHidemaruWindow, byte[] encode_data);
+        delegate int TOutputPane_Push(IntPtr hHidemaruWindow);
+        delegate int TOutputPane_Pop(IntPtr hHidemaruWindow);
+        delegate IntPtr TOutputPane_GetWindowHandle(IntPtr hHidemaruWindow);
+        delegate int TOutputPane_SetBaseDir(IntPtr hHidemaruWindow, byte[] encode_data);
+
+        static TOutputPane_Output pOutputPane_Output;
+        static TOutputPane_Push pOutputPane_Push;
+        static TOutputPane_Pop pOutputPane_Pop;
+        static TOutputPane_GetWindowHandle pOutputPane_GetWindowHandle;
+        static TOutputPane_SetBaseDir pOutputPane_SetBaseDir;
 
         [DllImport("kernel32.dll", SetLastError = true)]
         private static extern IntPtr GlobalLock(IntPtr hMem);
@@ -57,6 +73,7 @@ public sealed partial class hmLmDynamicLib
 
         // 秀丸本体のexeを指すモジュールハンドル
         static UnManagedDll hmExeHandle;
+        static UnManagedDll hmOutputPaneHandle;
 
         // 秀丸本体のExport関数を使えるようにポインタ設定。
         static void SetUnManagedDll()
@@ -79,12 +96,32 @@ public sealed partial class hmLmDynamicLib
                     if (_ver >= 873)
                     {
                         pGetCursorPosUnicodeFromMousePos = hmExeHandle.GetProcDelegate<TGetCursorPosUnicodeFromMousePos>("Hidemaru_GetCursorPosUnicodeFromMousePos");
+                        pGetCurrentWindowHandle = hmExeHandle.GetProcDelegate<TGetCurrentWindowHandle>("Hidemaru_GetCurrentWindowHandle");
                     }
 
                     if (_ver >= 890)
                     {
                         pAnalyzeEncoding = hmExeHandle.GetProcDelegate<TAnalyzeEncoding>("Hidemaru_AnalyzeEncoding");
                         pLoadFileUnicode = hmExeHandle.GetProcDelegate<TLoadFileUnicode>("Hidemaru_LoadFileUnicode");
+                    }
+
+                    try
+                    {
+                        string exedir = System.IO.Path.GetDirectoryName(strExecuteFullpath);
+                        hmOutputPaneHandle = new UnManagedDll(exedir + @"\HmOutputPane.dll");
+                        pOutputPane_Output = hmOutputPaneHandle.GetProcDelegate<TOutputPane_Output>("Output");
+                        pOutputPane_Push = hmOutputPaneHandle.GetProcDelegate<TOutputPane_Push>("Push");
+                        pOutputPane_Pop = hmOutputPaneHandle.GetProcDelegate<TOutputPane_Pop>("Pop");
+                        pOutputPane_GetWindowHandle = hmOutputPaneHandle.GetProcDelegate<TOutputPane_GetWindowHandle>("GetWindowHandle");
+
+                        if (_ver >= 877)
+                        {
+                            pOutputPane_SetBaseDir = hmOutputPaneHandle.GetProcDelegate<TOutputPane_SetBaseDir>("SetBaseDir");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        OutputDebugStream(ErrorMsg.MethodNeedOutputNotFound + ":\n" + e.Message);
                     }
 
                 }
--- win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmStaticLib.cs
+++ win-hidemaru-app/hm_loadclr/hmLoadCLR.src/NeoLua/hmLmStaticLib/hmLmStaticLib.cs
@@ -147,6 +147,10 @@ public sealed partial class hmLmDynamicLib
     [DllImport("kernel32.dll", SetLastError = true)]
     private static extern ulong GetModuleFileName(IntPtr hModule, StringBuilder filename, ulong bufsize);
 
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, int command, IntPtr lparam);
+
+
     public static Lua l;
     public static LuaGlobal g;
 
