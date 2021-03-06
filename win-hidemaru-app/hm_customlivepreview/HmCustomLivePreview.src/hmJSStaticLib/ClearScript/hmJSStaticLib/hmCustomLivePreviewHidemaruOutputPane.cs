73ed6bfae6b2af6f6556d06689b198587c784f6a
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2021-03-07 01:51:59 +0900

    アウトプット枠関連追加

--- win-hidemaru-app/hm_customlivepreview/HmCustomLivePreview.src/hmJSStaticLib/ClearScript/hmJSStaticLib/hmCustomLivePreviewHidemaruOutputPane.cs
+++ win-hidemaru-app/hm_customlivepreview/HmCustomLivePreview.src/hmJSStaticLib/ClearScript/hmJSStaticLib/hmCustomLivePreviewHidemaruOutputPane.cs
@@ -22,12 +22,62 @@ public sealed partial class HmCustomLivePreviewDynamicLib
             }
 
 
-            // Output枠へと出力する
-            public static int Output(string message)
+            public static int Output(object message)
             {
+                bool isClearScriptItem = false;
                 try
                 {
-                    byte[] encode_data = HmOriginalEncodeFunc.EncodeWStringToOriginalEncodeVector(message);
+                    if (message.GetType().Name == "WindowsScriptItem" || message.GetType().BaseType?.Name == "WindowsScriptItem" || message.GetType().BaseType?.BaseType?.Name == "WindowsScriptItem")
+                    {
+                        isClearScriptItem = true;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                string str_message = "";
+                // ClearScriptエンジンのオブジェクトであれば、そのまま出しても意味が無いので…
+                if (isClearScriptItem)
+                {
+                    dynamic dexp = message;
+
+                    // JSON.stringifyで変換できるはずだ
+                    try
+                    {
+                        str_message = engine.Script.JSON.stringify(dexp);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+
+                    // JSON.stringfyで空っぽだったようだ。
+                    if (str_message == String.Empty)
+                    {
+                        try
+                        {
+                            // ECMAScriptのtoString()で変換出来るはずだ…
+                            str_message = dexp.toString();
+                        }
+                        catch (Exception)
+                        {
+                            // 変換できなかったら、とりあえず、しょうがないので.NETのToStringで。多分意味はない。
+                            str_message = message.ToString();
+                        }
+                    }
+                }
+
+                // WindowsScriptItemオブジェクトでないなら、普通にToString
+                else
+                {
+                    str_message = message.ToString();
+                }
+
+                try
+                {
+                    byte[] encode_data = HmOriginalEncodeFunc.EncodeWStringToOriginalEncodeVector(str_message);
                     int result = pOutputPane_Output(Hidemaru.WindowHandle, encode_data);
                     return result;
                 }
