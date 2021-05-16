6c22edf5c42a7abf20ea3db48bf78b572df39515
komiyamma <komiyama0629@gmail.com>
2021-05-16 10:16:11 +0900

    不正になる警告に asyncio を含めるようにした

--- win-hidemaru-app/hm_python3/hmPython3.src/hmPython3/exception_translator.cpp
+++ win-hidemaru-app/hm_python3/hmPython3.src/hmPython3/exception_translator.cpp
@@ -16,7 +16,7 @@ std::wstring python_critical_exception_message() {
 		"hmPython3で致命的エラーが発生しました。\n"
 		"\n"
 		"・ライブラリの不正な多重解放\n"
-		"・複数回の初期化に対応していないパッケージの利用(numpy/pythonnet等)\n"
+		"・複数回の初期化に対応していないパッケージの利用(asyncio/numpy/pythonnet等)\n"
 		"\n"
 		"が疑われます。\n\n"
 		"「不正なメモリ状態」となったため、プロセスは正常に継続使用できません。\n"
