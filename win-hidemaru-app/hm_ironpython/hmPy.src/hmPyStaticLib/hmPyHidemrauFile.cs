70f7989b655ed17c19cb693c65d2b0d0d994e304
komiyamma <komiyama0629@gmail.com>
2021-05-17 21:23:09 +0900

    HmExplorer追加

--- win-hidemaru-app/hm_ironpython/hmPy.src/hmPyStaticLib/hmPyHidemrauFile.cs
+++ win-hidemaru-app/hm_ironpython/hmPy.src/hmPyStaticLib/hmPyHidemrauFile.cs
@@ -486,7 +486,7 @@ public sealed partial class hmPyDynamicLib
                         String text = Hidemaru.File.ReadAllText(this.m_path, this.m_encoding.HmEncode);
                         return text;
                     }
-                    catch (Exception e)
+                    catch (Exception )
                     {
                         throw;
                     }
