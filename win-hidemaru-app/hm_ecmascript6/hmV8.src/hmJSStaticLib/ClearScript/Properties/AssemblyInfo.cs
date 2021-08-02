4143d63199cee154d3bf99de4fd85f3444f07564
komiyamma <komiyama0629@gmail.com>
2021-08-02 23:58:47 +0900

    hmV8 1.9.0.2 としての公開物

--- win-hidemaru-app/hm_ecmascript6/hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_ecmascript6/hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.0.1")]
-[assembly: AssemblyFileVersion("1.9.0.1")]
+[assembly: AssemblyVersion("1.9.0.2")]
+[assembly: AssemblyFileVersion("1.9.0.2")]
