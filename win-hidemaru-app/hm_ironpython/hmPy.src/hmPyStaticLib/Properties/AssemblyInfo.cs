d4a9614850b83f6c42663cdfa0fbfdfe59583294
komiyamma <komiyama0629@gmail.com>
2021-08-10 02:01:45 +0900

    1.9.2.1 として公開したもの

--- win-hidemaru-app/hm_ironpython/hmPy.src/hmPyStaticLib/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_ironpython/hmPy.src/hmPyStaticLib/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 // 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
 [assembly: Guid("5d16c56b-9442-48a0-bcb7-86f7fa32f698")]
 
-[assembly: AssemblyVersion("1.9.0.1")]
-[assembly: AssemblyFileVersion("1.9.0.1")]
+[assembly: AssemblyVersion("1.9.2.1")]
+[assembly: AssemblyFileVersion("1.9.2.1")]
