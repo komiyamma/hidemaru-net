3d99d773caed89be6059b4dcb4b6e41ad76e13fd
komiyamma <komiyama0629@gmail.com>
2021-08-09 19:55:55 +0900

    コミット

--- win-hidemaru-app/hm_ecmascript6/hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
+++ win-hidemaru-app/hm_ecmascript6/hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.0.2")]
-[assembly: AssemblyFileVersion("1.9.0.2")]
+[assembly: AssemblyVersion("1.9.2.1")]
+[assembly: AssemblyFileVersion("1.9.2.1")]
