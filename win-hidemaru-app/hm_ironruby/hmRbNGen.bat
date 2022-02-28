ffe502c59ad20110b8f20e7049477174057a551f
komiyamma <komiyama0629@gmail.com>
2022-02-28 23:22:05 +0900

    hidemaru→hm_ironrubyへと移動

--- win-hidemaru-app/hm_ironruby/hmRbNGen.bat
+++
@@ -1,6 +0,0 @@
-cd %windir%\Microsoft.NET\Framework\v4.0.30319
-ngen install "C:\usr\hidemaru\hmRb.dll"
-ngen install "C:\usr\hidemaru\IronRuby.Libraries.dll"
-
-cd %windir%\Microsoft.NET\Framework64\v4.0.30319
-ngen install "C:\usr\hidemaru\IronRuby.Libraries.dll"
