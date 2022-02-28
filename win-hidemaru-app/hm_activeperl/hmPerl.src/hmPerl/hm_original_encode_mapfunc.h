ee88be0a53a7756446ee011b0bfa6cab6a4050d0
komiyamma <komiyama0629@gmail.com>
2022-03-01 03:46:07 +0900

    hidemaru-net → hm_activeperl へ移動

--- win-hidemaru-app/hm_activeperl/hmPerl.src/hmPerl/hm_original_encode_mapfunc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <string>
-#include <vector>
-
-// この結果のバイト列(vector.data())を HmOutputPane.dllのOutput関数になげれば、Unicodeでも再現できる
-std::vector<BYTE> EncodeWStringToOriginalEncodeVector(std::wstring original_string);
-
--- win-hidemaru-app/hm_javavm/hmJavaVM/java_CallStaticStringMethod.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <jni.h>
-#include <string>
-
-extern std::string CallStaticStringMethod(JNIEnv* env, const char* className, const char* methodName, int intArg);
-
