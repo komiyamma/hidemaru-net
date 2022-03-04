df9c804e66dd07013ed4f0cc60179bdd832eec41
komiyamma <komiyama0629@gmail.com>
2022-03-04 21:58:01 +0900

    引っ越し前のディレクトリ変更

---
+++ win-hidemaru-app/hm_selected_textautohighlighter/HmSelectedTextAutoHighlighter.src/HmSelectedTextAutoHighlighter/HmSelectedTextAutoHighlighterForm.cs
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2017 Akitsugu Komiyama
+ * under the MIT License
+ */
+
+using Hidemaru;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+public class HmSelectedTextAutoHighlighterForm : Form
+{
+    public static HmSelectedTextAutoHighlighterForm form;
+    private Timer timer;
+    private const int KeepMonitoringMilliSeconds = 1000; // 監視する上で、文字列選択が「固定した」とする目安時間。これプラス
+    private const int ItvlMonitoringMilliSeconds = 300; // 基本的な監視の「チェック」間隔
+    private const int mustMonitoringCount = KeepMonitoringMilliSeconds / ItvlMonitoringMilliSeconds;  // モニターすべきカウント数
+
+    private List<string> bufferSelectedText = new List<string>();
+    private string strLastHighlightSelectedText = "";
+
+    public HmSelectedTextAutoHighlighterForm()
+    {
+        SetTimerAttribute();
+    }
+
+    private void SetTimerAttribute()
+    {
+        timer = new Timer()
+        {
+            Interval = ItvlMonitoringMilliSeconds,
+            Enabled = true,
+        };
+
+        timer.Tick += timer_Tick;
+
+        timer.Start();
+    }
+
+    // 秀丸のマーカー状態を更新する
+    private bool TryUpdateHidemaruMarker(string text)
+    {
+        if (!Hm.Macro.IsExecuting)
+        {
+            // テキストを選択していない、もしくは、テキストが長すぎる
+            if (text.Length == 0 || text.Length > 100)
+            {
+                var ret = Hm.Macro.Exec.Eval(
+                @"
+                    deletecolormarker ""HmSelectedTextTmpColorMarker"", 33001;
+                    endmacro ""CompleteClear"";
+                ");
+
+                if (ret.Message == "CompleteClear")
+                {
+                    return true;
+                }
+
+            }
+            else
+            {
+
+                var ret = Hm.Macro.Exec.Eval(
+                @"
+                    setcompatiblemode 0x20000;
+                    deletecolormarker ""HmSelectedTextTmpColorMarker"", 33001;
+                    $text = gettext(seltopx, seltopy, selendx, selendy, 1);
+                    setsearch $text, 0x00000002;
+                    colormarkerallfound 0x333333,0x8CE6F0,-1,1,33001,""HmSelectedTextTmpColorMarker"";
+                    endmacro ""CompleteHighlight"";
+                ");
+
+                if (ret.Message == "CompleteHighlight")
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // 選択しているテキストが「安定した」という条件
+    private bool IsConditionMonitoringPass(String text)
+    {
+
+        if (bufferSelectedText.Count >= mustMonitoringCount)
+        {
+            if (bufferSelectedText.TrueForAll(t => t == text))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // 選択しているテキストをTickごとに蓄積するためのQueueにためこむ
+    private void AddQueueBufferSelectedText(String text)
+    {
+        // バッファーに加える
+        bufferSelectedText.Add(text);
+
+        // モニターすべきカウント数より、バッファーのカウントが多かったら、先頭から取り除く
+        while (true)
+        {
+            if (bufferSelectedText.Count > mustMonitoringCount)
+            {
+                bufferSelectedText.RemoveAt(0);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    private void timer_Tick(object sender, EventArgs e)
+    {
+        try
+        {
+            var text = Hm.Edit.SelectedText;
+
+            AddQueueBufferSelectedText(text);
+
+            if (text != strLastHighlightSelectedText)
+            {
+                if (IsConditionMonitoringPass(text))
+                {
+                    if (TryUpdateHidemaruMarker(text))
+                    {
+                        strLastHighlightSelectedText = text;
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Trace.WriteLine(ex);
+        }
+    }
+
+    public void Stop()
+    {
+        timer.Enabled = false;
+        timer.Stop();
+        timer = null;
+    }
+}
---
+++ win-hidemaru-app/hm_selected_textautohighlighter/HmSelectedTextAutoHighlighter.src/HmSelectedTextAutoHighlighter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("HmSelectedTextAutoHighlighter for .NET 4.5 ANYCPU")]
+[assembly: AssemblyDescription("HmSelectedTextAutoHighlighter for .NET 4.5 ANYCPU")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HmSelectedTextAutoHighlighter for .NET 4.5 ANYCPU")]
+[assembly: AssemblyCopyright("Copyright (c) 2017-2019 Akitsugu Komiyama")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("86032b43-024a-4162-9ba5-0f488e39f288")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 以下のように '*' を使用します:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
