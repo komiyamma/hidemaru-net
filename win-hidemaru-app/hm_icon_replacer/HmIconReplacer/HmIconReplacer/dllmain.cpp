89e6d87255e54725e3ca78f2c08669dec2e0a573
komiyamma <komiyama0629@gmail.com>
2022-03-04 17:21:38 +0900

    hidemaru-net から hm_markdownpreview へのお引越し

--- win-hidemaru-app/hm_icon_replacer/HmIconReplacer/HmIconReplacer/dllmain.cpp
+++ win-hidemaru-app/hm_icon_replacer/HmIconReplacer/HmIconReplacer/dllmain.cpp
@@ -12,53 +12,56 @@ wchar_t wszHidemaruFullPath[MAX_PATH] = L"";
 HMODULE hHideExeHandle = NULL;
 HWND hCurrentWindowHandle = NULL;
 HWND GetCurrentHidemaruWindowHandle() {
-    if (hCurrentWindowHandle) {
-        return hCurrentWindowHandle;
-    }
-    if (!hHideExeHandle) {
-        // 秀丸本体に関数があるのは 8.73.99以上
-        GetModuleFileName(NULL, wszHidemaruFullPath, _countof(wszHidemaruFullPath));
-        hHideExeHandle = LoadLibrary(wszHidemaruFullPath);
-        if (hHideExeHandle) {
-            Hidemaru_GetCurrentWindowHandle = (PFNGetCurrentWindowHandle)GetProcAddress(hHideExeHandle, "Hidemaru_GetCurrentWindowHandle");
-        }
-    }
-    if (Hidemaru_GetCurrentWindowHandle) {
-        hCurrentWindowHandle = Hidemaru_GetCurrentWindowHandle();
-        return hCurrentWindowHandle;
-    }
+	if (hCurrentWindowHandle) {
+		return hCurrentWindowHandle;
+	}
+	if (!hHideExeHandle) {
+		// 秀丸本体に関数があるのは 8.73.99以上
+		GetModuleFileName(NULL, wszHidemaruFullPath, _countof(wszHidemaruFullPath));
+		hHideExeHandle = LoadLibrary(wszHidemaruFullPath);
+		if (hHideExeHandle) {
+			Hidemaru_GetCurrentWindowHandle = (PFNGetCurrentWindowHandle)GetProcAddress(hHideExeHandle, "Hidemaru_GetCurrentWindowHandle");
+		}
+	}
+	if (Hidemaru_GetCurrentWindowHandle) {
+		hCurrentWindowHandle = Hidemaru_GetCurrentWindowHandle();
+		return hCurrentWindowHandle;
+	}
 
-    return NULL;
+	return NULL;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH: {
-        HICON hIcon = LoadIcon(hModule, MAKEINTRESOURCE(IDI_ICON1));
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH: {
+		HICON hIcon = LoadIcon(hModule, MAKEINTRESOURCE(IDI_ICON1));
 
-        HWND hwnd = FindWindow(L"Hidemaru32Class", NULL);
-        if (hwnd) {
-            //Change both icons to the same icon handle.
-            SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
-            SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+		HWND hwnd = FindWindow(L"Hidemaru32Class", NULL);
+		if (!hwnd) {
+			hwnd = FindWindow(L"Hidemaru32Class_Appx", NULL); 
+		}
+		if (hwnd) {
+			//Change both icons to the same icon handle.
+			SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+			SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
 
-            //This will ensure that the application icon gets changed too.
-            SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
-            SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-        }
+			//This will ensure that the application icon gets changed too.
+			SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+			SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+		}
 
-        break;
-    }
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
 }
 
