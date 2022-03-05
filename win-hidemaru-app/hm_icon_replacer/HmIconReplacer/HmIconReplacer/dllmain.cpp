b6c623ce2f11bb3bf4e0a3eb88f3ff47146d9487
komiyamma <komiyama0629@gmail.com>
2022-03-06 00:26:50 +0900

    アイコンを設定するもの、戻すものの２つに関数分離

--- win-hidemaru-app/hm_icon_replacer/HmIconReplacer/HmIconReplacer/dllmain.cpp
+++ win-hidemaru-app/hm_icon_replacer/HmIconReplacer/HmIconReplacer/dllmain.cpp
@@ -3,6 +3,7 @@
 #include "resource.h"
 #include "windows.h"
 #include <memory>
+#include "dllmain.h"
 
 // 秀丸のウィンドウハンドル
 using PFNGetCurrentWindowHandle = HWND(WINAPI*)();
@@ -31,39 +32,8 @@ HWND GetCurrentHidemaruWindowHandle() {
 	return NULL;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+void ChangeHidemaruIcon(const HICON hIcon)
 {
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH: {
-		HICON hIcon = LoadIcon(hModule, MAKEINTRESOURCE(IDI_ICON1));
-
-		HWND hwnd = FindWindow(L"Hidemaru32Class", NULL);
-		if (hwnd) {
-			//Change both icons to the same icon handle.
-			SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
-			SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-
-			//This will ensure that the application icon gets changed too.
-			SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
-			SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
-		}
-
-		break;
-	}
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
-	return TRUE;
-}
-
-extern "C" __declspec(dllexport) BOOL RestoreDefaultIcon()
-{
-	HMODULE hideHandle = GetModuleHandle(NULL);
-	HICON hIcon = LoadIcon(hideHandle, MAKEINTRESOURCE(IDI_ICON1));
-
 	HWND hwnd = FindWindow(L"Hidemaru32Class", NULL);
 	if (hwnd) {
 		//Change both icons to the same icon handle.
@@ -74,6 +44,42 @@ extern "C" __declspec(dllexport) BOOL RestoreDefaultIcon()
 		SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
 		SendMessage(GetWindow(hwnd, GW_OWNER), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
 	}
+}
+
+HMODULE dllHandle = NULL;
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH: {
+		dllHandle = hModule;
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
+extern "C" __declspec(dllexport) BOOL ChangeNewCoolIcon()
+{
+	if (dllHandle) {
+		HICON hIcon = LoadIcon(dllHandle, MAKEINTRESOURCE(IDI_ICON1));
+		ChangeHidemaruIcon(hIcon);
+	}
+
+	return TRUE;
+}
+
+extern "C" __declspec(dllexport) BOOL ChangeDefaultIcon()
+{
+	HMODULE hideHandle = GetModuleHandle(NULL);
+	HICON hIcon = LoadIcon(hideHandle, MAKEINTRESOURCE(IDI_ICON1));
+	ChangeHidemaruIcon(hIcon);
+
 	return TRUE;
 }
 
